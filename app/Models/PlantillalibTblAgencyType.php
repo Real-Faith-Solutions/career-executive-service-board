c964956a9fca8de5ffba1ba01224f937dc06b0de
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-06-28 16:45:57 +0800

    plantilla migration another fix

--- app/Models/PlantillalibTblAgencyType.php
+++ app/Models/PlantillalibTblAgencyType.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class PlantillalibTblAgencyType extends Model
 {
     use HasFactory;
+
+    protected $table = "plantillalib_tblAgencyType";
+
 }
--- database/migrations/plantilla/2023_05_07_144230_create_plantillalib_tblAgencyType_table.php
+++ database/migrations/plantilla/2023_05_07_144230_create_plantillalib_tblAgencyType_table.php
@@ -8,18 +8,20 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * PlantillalibTblAgencyType
      */
     public function up(): void
     {
-        Schema::create('plantillalib_tbl_agency_types', function (Blueprint $table) {
-            $table->id();
-            $table->string('agency_typeid')->nullable();
+        Schema::create('plantillalib_tblAgencyType', function (Blueprint $table) {
+            $table->id('agency_typeid');
+            // $table->string('agency_typeid')->nullable();
             $table->string('sectorid')->nullable();
             $table->string('title')->nullable();
             $table->string('encdate')->nullable();
             $table->string('lastupd_dt')->nullable();
             $table->string('encoder')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('plantillalib_tbl_agency_types');
+        Schema::dropIfExists('plantillalib_tblAgencyType');
     }
 };
