e49ee58e3506ad1e38c7a5894fbff75bc1ba825b
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-29 10:17:48 +0800

    update main content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
 
     <div class="p-4 sm:ml-64">
-        <div class="mt-14 rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
+        <div class="mt-14 rounded-lg p-4">
             @yield('content')
         </div>
     </div>
