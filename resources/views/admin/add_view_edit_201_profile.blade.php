f3768f04ed670175e40b88ebc2c453e26e171383
koykoy027 <villanuevajoshua27@gmail.com>
2023-07-03 12:57:41 +0800

    add hidden class in each category

--- resources/views/admin/add_view_edit_201_profile.blade.php
+++ resources/views/admin/add_view_edit_201_profile.blade.php
@@ -92,71 +92,71 @@
 
         @if (str_contains(Request::url(), 'profile/view'))
 
-            <div class="category familyProfileTab">
+            <div class="category familyProfileTab hidden">
                 @include('admin.201_profiling.partials.family_profile')
             </div>
 
-            <div class="category educationalAttainmentTab">
+            <div class="category educationalAttainmentTab hidden">
                 @include('admin.201_profiling.partials.educational_attainment')
             </div>
 
-            <div class="category examinationsTakenTab">
+            <div class="category examinationsTakenTab hidden">
                 @include('admin.201_profiling.partials.examinations_taken')
             </div>
 
-            <div class="category languagesDialectsTab">
+            <div class="category languagesDialectsTab hidden">
                 @include('admin.201_profiling.partials.languages_dialects')
             </div>
 
-            <div class="category eligibilityAndRankTrackerTab">
+            <div class="category eligibilityAndRankTrackerTab hidden">
                 @include('admin.201_profiling.partials.eligibility_and_rank_tracker')
             </div>
 
-            <div class="category recordOfCespesRatingHrTab">
+            <div class="category recordOfCespesRatingHrTab hidden">
                 @include('admin.201_profiling.partials.record_of_cespes_rating_hr')
             </div>
 
-            <div class="category workExperienceTab">
+            <div class="category workExperienceTab hidden">
                 @include('admin.201_profiling.partials.work_experience')
             </div>
 
-            <div class="category fieldExpertiseTab">
+            <div class="category fieldExpertiseTab hidden">
                 @include('admin.201_profiling.partials.field_expertise')
             </div>
 
-            <div class="category cesTrainingsTab">
+            <div class="category cesTrainingsTab hidden">
                 @include('admin.201_profiling.partials.ces_trainings')
             </div>
 
-            <div class="category otherManagementTrainingsTab">
+            <div class="category otherManagementTrainingsTab hidden">
                 @include('admin.201_profiling.partials.other_management_trainings')
             </div>
 
-            <div class="category researchAndStudiesTab">
+            <div class="category researchAndStudiesTab hidden">
                 @include('admin.201_profiling.partials.research_and_studies')
             </div>
 
-            <div class="category scholarshipsTab">
+            <div class="category scholarshipsTab hidden">
                 @include('admin.201_profiling.partials.scholarships')
             </div>
 
-            <div class="category majorCivicAndProfessionalAffiliationsTab">
+            <div class="category majorCivicAndProfessionalAffiliationsTab hidden">
                 @include('admin.201_profiling.partials.major_civic_and_professional_affiliations')
             </div>
 
-            <div class="category awardAndCitationsTab">
+            <div class="category awardAndCitationsTab hidden">
                 @include('admin.201_profiling.partials.award_and_citations')
             </div>
 
-            <div class="category caseRecordsTab">
+            <div class="category caseRecordsTab hidden">
                 @include('admin.201_profiling.partials.case_records')
             </div>
 
-            <div class="category healthRecordsTab">
+            <div class="category healthRecordsTab hidden">
                 @include('admin.201_profiling.partials.health_records')
             </div>
 
-            <div class="category pdfFilesTab">
+            <div class="category pdfFilesTab hidden">
                 @include('admin.201_profiling.partials.pdf_files')
             </div>
 
