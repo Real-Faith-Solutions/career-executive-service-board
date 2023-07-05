c6f51089d20beb5c3324a2e39f77c0464e541c02
koykoy027 <villanuevajoshua27@gmail.com>
2023-07-05 16:10:17 +0800

    address models and seeders

---
+++ database/seeders/ProfileAddress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ProfileAddress as ModelsProfileAddress;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Factory as Faker;
+
+class ProfileAddress extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(): void
+    {
+        $faker = Faker::create();
+        ModelsProfileAddress::create([
+            'cesno' => '1',
+            'type' => $faker->randomElement(['Permanent address', 'Mailing address', 'Temporary address']),
+            'floor_bldg' => $faker->address,
+            'no_street' => $faker->address,
+            'brgy_or_district' => $faker->state,
+            'city_or_municipality' => $faker->city,
+            'zip_code' => $faker->postcode,
+            'encoder'  => $faker->name,
+            'last_updated_by' => $faker->dateTime,
+        ]);
+    }
+}
