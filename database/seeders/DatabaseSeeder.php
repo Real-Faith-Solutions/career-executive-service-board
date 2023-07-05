b140a597cdc416a82070853eb8f26a1c39ebee80
koykoy027 <villanuevajoshua27@gmail.com>
2023-07-05 11:15:21 +0800

    add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             CesWebAppGeneralPageAccessesTableSeeder::class,
+            ProfileData::class,
         ]);
-        
+
     }
 }
