5069e44b386cd5e35c37aa5e3c2b92f760a4e20f
koykoy027 <villanuevajoshua27@gmail.com>
2023-07-05 16:16:41 +0800

    add address for category

---
+++ resources/views/admin/201_profiling/view_profile/partials/address.blade.php
@@ -0,0 +1,50 @@
+<div class="my-5 flex justify-end">
+    <button class="btn btn-primary">Add Address</button>
+</div>
+
+<div class="relative overflow-x-auto sm:rounded-lg shadow-lg">
+    <table class="w-full text-left text-sm text-gray-500">
+        <thead class="bg-blue-500 text-xs uppercase text-gray-700 text-white">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Level
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    School
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    School Type
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Period of Attendance
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Action</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr class="border-b bg-white">
+                <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
+                    Elementary
+                </td>
+                <td class="px-6 py-3">
+                    Bagong Silang High school
+                </td>
+                <td class="px-6 py-3">
+                    Local
+                </td>
+                <td class="px-6 py-3">
+                    2019
+                </td>
+
+                <td class="px-6 py-4 text-right uppercase">
+                    <a href="#" class="mx-1 font-medium text-blue-600 hover:underline">Update</a>
+                    <a href="#" class="mx-1 font-medium text-red-600 hover:underline">Delete</a>
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
