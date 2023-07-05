d5eda7834476fc9817c1334ad5bb78268539b85f
koykoy027 <villanuevajoshua27@gmail.com>
2023-07-05 08:37:21 +0800

    family profile table

--- resources/views/admin/201_profiling/partials/family_profile.blade.php
+++ resources/views/admin/201_profiling/partials/family_profile.blade.php
@@ -4,248 +4,10 @@
 </div>
 
 <div class="mb-3 bg-blue-500 p-2 uppercase text-white">
-    <h1>Spouse Details</h1>
+    <h1>Family Details</h1>
 </div>
 
 <div class="relative overflow-x-auto sm:rounded-lg">
-    <table class="w-full text-left text-sm text-gray-500">
-        <thead class="bg-gray-50 text-xs uppercase text-gray-700">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Spouse Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Occupation
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Employer/Business Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Employer/Business Address
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Action</span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <tr class="border-b bg-white">
-                <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="mx-1 font-medium text-blue-600 hover:underline">Update</a>
-                    <a href="#" class="mx-1 font-medium text-red-600 hover:underline">Delete</a>
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
-</div>
-
-{{-- children records --}}
-<div class="mb-3 bg-blue-500 p-2 uppercase text-white">
-    <h1>Children Records</h1>
-</div>
-
-<div class="relative overflow-x-auto sm:rounded-lg">
-    <table class="w-full text-left text-sm text-gray-500">
-        <thead class="bg-gray-50 text-xs uppercase text-gray-700">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Last Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    First Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Middle Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Name Extension
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Birthdate
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Birthplace
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Action</span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <tr class="border-b bg-white">
-                <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="mx-1 font-medium text-blue-600 hover:underline">Update</a>
-                    <a href="#" class="mx-1 font-medium text-red-600 hover:underline">Delete</a>
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
-</div>
-
-{{-- father's name details --}}
-<div class="mb-3 bg-blue-500 p-2 uppercase text-white">
-    <h1>Father's Name</h1>
-</div>
-
-<div class="relative overflow-x-auto sm:rounded-lg">
-    <table class="w-full text-left text-sm text-gray-500">
-        <thead class="bg-gray-50 text-xs uppercase text-gray-700">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Last Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    First Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Middle Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Name Extension
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Action</span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <tr class="border-b bg-white">
-                <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="mx-1 font-medium text-blue-600 hover:underline">Update</a>
-                    <a href="#" class="mx-1 font-medium text-red-600 hover:underline">Delete</a>
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
-</div>
-
-{{-- mother's name details --}}
-<div class="mb-3 bg-blue-500 p-2 uppercase text-white">
-    <h1>Mother's Name</h1>
-</div>
-
-<div class="relative overflow-x-auto sm:rounded-lg">
-    <table class="w-full text-left text-sm text-gray-500">
-        <thead class="bg-gray-50 text-xs uppercase text-gray-700">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Last Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    First Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    Middle Name
-                </th>
-
-                <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Action</span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <tr class="border-b bg-white">
-                <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
-                    Lorem ipsum dolor
-                </td>
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-                <td class="px-6 py-3">
-                    Lorem ipsum dolor
-                </td>
-
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="mx-1 font-medium text-blue-600 hover:underline">Update</a>
-                    <a href="#" class="mx-1 font-medium text-red-600 hover:underline">Delete</a>
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
-</div>
-
-
-{{-- <div class="relative overflow-x-auto sm:rounded-lg">
     <table class="w-full text-left text-sm text-gray-500">
         <thead class="bg-gray-50 text-xs uppercase text-gray-700">
             <tr>
@@ -253,11 +15,21 @@
                     Relationship
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Name
+                    Lastname
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Occupation
+                    Firstname
                 </th>
+
+                <th scope="col" class="px-6 py-3">
+                    Extension name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Middlename
+                </th>
+
+
+
                 <th scope="col" class="px-6 py-3">
                     <span class="sr-only">Action</span>
                 </th>
@@ -267,16 +39,24 @@
 
             <tr class="border-b bg-white">
                 <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
-                    Father
+                    Spouse
                 </td>
+                <td class="px-6 py-3">
+                    Spouse name
+                </td>
+
+                <td class="px-6 py-3">
+                    Lorem ipsum dolor
+                </td>
+
                 <td class="px-6 py-3">
                     Lorem ipsum dolor
                 </td>
                 <td class="px-6 py-3">
-                    Single Mom
+                    Lorem ipsum dolor
                 </td>
 
-                <td class="px-6 py-4 text-right">
+                <td class="px-6 py-4 text-right uppercase">
                     <a href="#" class="mx-1 font-medium text-blue-600 hover:underline">Update</a>
                     <a href="#" class="mx-1 font-medium text-red-600 hover:underline">Delete</a>
                 </td>
@@ -284,4 +64,4 @@
 
         </tbody>
     </table>
-</div> --}}
+</div>
