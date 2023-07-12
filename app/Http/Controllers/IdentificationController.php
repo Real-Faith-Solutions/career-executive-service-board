d112bb815163befa5cc2bb400a775f67835ed360
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-07-12 10:28:34 +0800

    request validated for identification card

--- app/Http/Controllers/IdentificationController.php
+++ app/Http/Controllers/IdentificationController.php
@@ -5,17 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Identification;
 use App\Models\PersonalData;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IdentificationController extends Controller
 {
 
     public function store(Request $request, $cesno){
 
+        $request->validate([
+
+            'type' => ['required'],
+            'id_number' => ['required', 'min:2', 'max:40'],
+            
+        ]);
+
+        $userLastName = Auth::user()->last_name;
+
         $identification = new Identification([
 
             'type' => $request->type,
             'id_number' => $request->identification_id,
-            'encoder' => 'sample encoder',
+            'encoder' => $userLastName,
          
         ]);
 
