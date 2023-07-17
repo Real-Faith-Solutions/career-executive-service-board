086edfbfb60ffa7444fa566fe916575b1a5b2985
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-07-17 11:08:01 +0800

    real-time validation for 201 avatar upload

--- public/js/profile-avatar.js
+++ public/js/profile-avatar.js
@@ -1,3 +1,7 @@
+let errorMessageAvatar = document.getElementById('ErrorMessageAvatar');
+let imageInputAvatar = document.getElementById('imageInputAvatar');
+let uploadButtonAvatar = document.getElementById('uploadButtonAvatar');
+
 // Open the modal when the image is clicked
 document.getElementById('profile-avatar').addEventListener('click', function() {
     showModal();
@@ -18,6 +22,10 @@ document.getElementById('profile-avatar').addEventListener('click', function() {
   // Show the modal
   function showModal() {
     document.getElementById('profile-avatar-modal').classList.remove('hidden');
+    if (errorMessageAvatar.textContent != '' || imageInputAvatar.value == '') {
+      uploadButtonAvatar.classList.add('cursor-not-allowed');
+      uploadButtonAvatar.classList.remove('cursor-pointer');
+    }
   }
   
   // Close the modal
@@ -26,22 +34,47 @@ document.getElementById('profile-avatar').addEventListener('click', function() {
   }
 
   // Submit the form when the upload button is clicked
-    document.getElementById('uploadButtonAvatar').addEventListener('click', function() {
-        document.getElementById('uploadFormAvatar').submit();
+    document.getElementById('uploadButtonAvatar').addEventListener('click', function(event) {
+        
+      if (errorMessageAvatar.textContent != '' || imageInputAvatar.value == '') {
+          event.preventDefault();
+          return;
+      }
+      
+      document.getElementById('uploadFormAvatar').submit();
     });
 
  // Image Preview on Avatar 
     document.getElementById('imageInputAvatar').addEventListener('change', function(event) {
-      var input = event.target;
-      var reader = new FileReader();
+
+      let file = imageInputAvatar.files[0];
+      let allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+      let imagePreview = document.getElementById('imagePreviewAvatar');
+
+      if (!allowedExtensions.test(file.name)) {
+          // alert('Please upload a valid image file with the extensions .jpg, .jpeg, or .png.');
+          this.value = '';
+          imagePreview.src = '';
+          imagePreview.classList.add('hidden');
+          errorMessageAvatar.textContent = "Please upload a valid image file with the extensions .jpg, .jpeg, or .png.";
+          uploadButtonAvatar.classList.add('cursor-not-allowed');
+          uploadButtonAvatar.classList.remove('cursor-pointer');
+          return;
+      }
+
+      let input = event.target;
+      let reader = new FileReader();
     
       reader.onload = function(){
-        var imagePreview = document.getElementById('imagePreviewAvatar');
         imagePreview.src = reader.result;
         imagePreview.classList.remove('hidden');
       };
-    
+
       if (input.files && input.files[0]) {
         reader.readAsDataURL(input.files[0]);
+        errorMessageAvatar.textContent = "";
+        uploadButtonAvatar.classList.remove('cursor-not-allowed');
+        uploadButtonAvatar.classList.add('cursor-pointer');
       }
+
     });
