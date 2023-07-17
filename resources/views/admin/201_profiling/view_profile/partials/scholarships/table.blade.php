cbbde608d7149ac4063a2032c0ed08d6897737a1
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-07-17 10:11:45 +0800

    update function for scholarship

--- resources/views/admin/201_profiling/view_profile/partials/scholarships/table.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/scholarships/table.blade.php
@@ -53,7 +53,12 @@
 
                     <td class="px-6 py-4 text-right uppercase">
                         <div class="flex">
-                            <a href="#" class="mx-1 font-medium text-blue-600 hover:underline">Update</a>
+                            <form action="{{ route('scholarship.edit', ['ctrlno'=>$scholarships->ctrlno]) }}" method="GET">
+                                @csrf
+                                <button class="mx-1 font-medium text-blue-600 hover:underline" type="submit">
+                                   UPDATE
+                                </button>
+                            </form>
                         
                             <form action="{{ route('scholarship.destroy', ['ctrlno'=>$scholarships->ctrlno]) }}" method="POST">
                                 @csrf
