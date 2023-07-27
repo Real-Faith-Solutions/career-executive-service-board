52bd679c7bfa794e3cf7b76cb435461edb499974
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-07-27 14:17:37 +0800

    award and citations done

--- resources/views/admin/201_profiling/view_profile/partials/award_and_citations/edit.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/award_and_citations/edit.blade.php
@@ -11,14 +11,15 @@
         </div>
 
         <div class="bg-white px-6 py-3">
-            <form action="{{ route('award-citation.update', ['ctrlno'=>$awardAndCitation->ctrlno]) }}" method="POST">
+            <form action="{{ route('award-citation.update', ['ctrlno'=>$awardAndCitation->ctrlno]) }}" method="POST" id="award_and_citations_edit" onsubmit="return checkErrorsBeforeSubmit(award_and_citations_edit)">
                 @csrf
                 @method('PUT')
                 
                 <div class="sm:gid-cols-1 mb-3 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                     <div class="mb-3">
-                        <label for="title_of_award">Title of Award<sup>*</sup></label>
-                        <input id="title_of_award" name="title_of_award" value="{{ $awardAndCitation->awards }}" required type="text">
+                        <label for="title_of_award_edit">Title of Award<sup>*</sup></label>
+                        <input type="text" id="title_of_award_edit" name="title_of_award" value="{{ $awardAndCitation->awards }}" oninput="validateInput(title_of_award_edit, 2, 'alphaNumeric')" onkeypress="validateInput(title_of_award_edit, 2, 'alphaNumeric')" onblur="checkErrorMessage(title_of_award_edit)" required>
+                        <p class="input_error text-red-600"></p>
                         @error('title_of_award')
                             <span class="invalid" role="alert">
                                 <p>{{ $message }}</p>
@@ -27,8 +28,9 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="sponsor">Sponsor<sup>*</sup></label>
-                        <input id="sponsor" name="sponsor" value="{{ $awardAndCitation->sponsor }}" required type="text">
+                        <label for="award_sponsor_edit">Sponsor<sup>*</sup></label>
+                        <input type="text" id="award_sponsor_edit" name="sponsor" value="{{ $awardAndCitation->sponsor }}" oninput="validateInput(award_sponsor_edit, 2, 'alphaNumeric')" onkeypress="validateInput(award_sponsor_edit, 2, 'alphaNumeric')" onblur="checkErrorMessage(award_sponsor_edit)" required>
+                        <p class="input_error text-red-600"></p>
                         @error('sponsor')
                             <span class="invalid" role="alert">
                                 <p>{{ $message }}</p>
@@ -37,8 +39,9 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="date">Date<sup>*</sup></label>
-                        <input id="date" name="date" value="{{ $awardAndCitation->date }}" required type="date">
+                        <label for="award_edit_date">Date<sup>*</sup></label>
+                        <input type="date" id="award_edit_date" name="date" value="{{ $awardAndCitation->date }}" oninput="validateDateInput(award_edit_date)" required>
+                        <p class="input_error text-red-600"></p>
                         @error('date')
                             <span class="invalid" role="alert">
                                 <p>{{ $message }}</p>
--- resources/views/admin/201_profiling/view_profile/partials/award_and_citations/form.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/award_and_citations/form.blade.php
@@ -7,13 +7,14 @@
         </div>
 
         <div class="bg-white px-6 py-3">
-            <form action="{{ route('award-citation.store', ['cesno' =>$mainProfile->cesno]) }}" method="POST">
+            <form action="{{ route('award-citation.store', ['cesno' =>$mainProfile->cesno]) }}" method="POST" id="award_and_citations_form" onsubmit="return checkErrorsBeforeSubmit(award_and_citations_form)">
                 @csrf
 
                 <div class="sm:gid-cols-1 mb-3 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                     <div class="mb-3">
                         <label for="title_of_award">Title of Award<sup>*</sup></label>
-                        <input id="title_of_award" name="title_of_award" required type="text">
+                        <input type="text" id="title_of_award" name="title_of_award" oninput="validateInput(title_of_award, 2, 'alphaNumeric')" onkeypress="validateInput(title_of_award, 2, 'alphaNumeric')" onblur="checkErrorMessage(title_of_award)" required>
+                        <p class="input_error text-red-600"></p>
                         @error('title_of_award')
                             <span class="invalid" role="alert">
                                 <p>{{ $message }}</p>
@@ -22,8 +23,9 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="sponsor">Sponsor<sup>*</sup></label>
-                        <input id="sponsor" name="sponsor" required type="text">
+                        <label for="award_sponsor">Sponsor<sup>*</sup></label>
+                        <input type="text" id="award_sponsor" name="sponsor" oninput="validateInput(award_sponsor, 2, 'alphaNumeric')" onkeypress="validateInput(award_sponsor, 2, 'alphaNumeric')" onblur="checkErrorMessage(award_sponsor)" required>
+                        <p class="input_error text-red-600"></p>
                         @error('sponsor')
                             <span class="invalid" role="alert">
                                 <p>{{ $message }}</p>
@@ -32,8 +34,9 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="date">Date<sup>*</sup></label>
-                        <input id="date" name="date" required type="date">
+                        <label for="award_date">Date<sup>*</sup></label>
+                        <input type="date" id="award_date" name="date" oninput="validateDateInput(award_date)" required>
+                        <p class="input_error text-red-600"></p>
                         @error('date')
                             <span class="invalid" role="alert">
                                 <p>{{ $message }}</p>
