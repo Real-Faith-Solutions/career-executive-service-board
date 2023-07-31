fc20f03021623d739636b75a717d27a8c18b46d8
koykoy027 <villanuevajoshua27@gmail.com>
2023-07-31 10:14:07 +0800

    plantilla_motherdept migration

--- database/migrations/2023_07_21_092158_create_plantilla_motherdept_table.php
+++ database/migrations/2023_07_21_092158_create_plantilla_motherdept_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('vw_plantilla_motherdept', function (Blueprint $table) {
+        Schema::create('plantilla_motherdept', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('encoder');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('vw_plantilla_motherdept');
+        Schema::dropIfExists('plantilla_motherdept');
     }
 };
