f5850e74f645539e0af2ba8a538f0eecaf32244f
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-08-06 21:41:16 +0800

    spouse confirm update done

--- resources/views/admin/201_profiling/view_profile/partials/family_profile/edit_spouse.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/family_profile/edit_spouse.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="bg-white px-6 py-3">
-            <form action="{{ route('family-profile.updateSpouseRecord', ['ctrlno'=>$spouseRecord->ctrlno]) }}" method="POST">
+            <form action="{{ route('family-profile.updateSpouseRecord', ['ctrlno'=>$spouseRecord->ctrlno]) }}" method="POST"  id="update_spouse_form" onsubmit="return checkErrorsBeforeSubmit(update_spouse_form)">
                 @csrf
                 @method('PUT')
                 
@@ -110,7 +110,7 @@
             
                 <div>
                     <div class="flex justify-end">
-                        <button type="submit" class="btn btn-primary">
+                        <button type="button" class="btn btn-primary" id="updateSpouseButton" onclick="openConfirmationDialog(this, 'Confirm Changes', 'Are you sure you want to update this info?')">
                             Update Changes
                         </button>
                     </div>
