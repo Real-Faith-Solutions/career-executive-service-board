846735937c1b9d134a0976eebba1ef87c81bd97a
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-08-19 20:37:30 +0800

    picture synced

--- app/Providers/AdminPermissionsComposerServiceProvider.php
+++ app/Providers/AdminPermissionsComposerServiceProvider.php
@@ -26,6 +26,7 @@ class AdminPermissionsComposerServiceProvider extends ServiceProvider
                         'userPermissions' => $userPermissions,
                         'userRole' => $userRole->role_name,
                         'user_cesno' => $personalData->cesno,
+                        'user_picture' => $personalData->picture,
                         'userName' => $personalData ? $personalData->firstname.' '.$personalData->lastname : null,
                         'userFirstName' => $personalData ? $personalData->firstname : null,
                         'userLastName' => $personalData ? $personalData->lastname : null,
