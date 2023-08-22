a554c0543ec35c3fe1a52968dc7c5bff5fb1de4a
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-08-22 15:57:16 +0800

    model and migration for specialization (expertise gen)

--- app/Models/ProfileLibTblExpertiseGen.php
+++ app/Models/ProfileLibTblExpertiseGen.php
@@ -4,15 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProfileLibTblExpertiseGen extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $primaryKey = 'GenExp_Code';
 
     protected $table = "profilelib_tblExpertiseGen";
 
     protected $fillable = [
-        'GenExp_Code',
         'Title',
     ]; 
 }
---
+++ resources/views/admin/competency/partials/training_type_library/expertise_specialization/table.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+@section('title', 'Field of Specialization')
+@section('sub', 'Field of Specialization')
+@section('content')
+@include('admin.competency.view_profile.header')
+
+<div class="my-5 flex justify-end">
+    <a href="">
+        <lord-icon
+            src="https://cdn.lordicon.com/jmkrnisz.json"
+            trigger="hover"
+            colors="primary:#DC3545"
+            style="width:34px;height:34px">
+      </lord-icon>
+    </a>
+
+    <a href="{{ route('training-secretariat.create') }}" class="btn btn-primary" >Add Specialization</a>
+</div>
+
+<div class="table-management-training relative overflow-x-auto sm:rounded-lg shadow-lg">
+    <table class="w-full text-left text-sm text-gray-500">
+        <thead class="bg-blue-500 text-xs uppercase text-gray-700 text-white">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Ctrlno
+                </th>
+
+                <th scope="col" class="px-6 py-3">
+                    Description
+                </th>
+
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Action</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- @foreach ($trainingSecretariat as $trainingSecretariats) --}}
+                <tr class="border-b bg-white">
+                    <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
+                        {{-- {{ $trainingSecretariats->ctrlno }} --}}
+                    </td>
+
+                    <td class="px-6 py-3">
+                        {{-- {{ $trainingSecretariats->description }} --}}
+                    </td>
+
+                    <td class="px-6 py-4 text-right uppercase">
+                        <div class="flex">
+                            {{-- <form action="{{ route('training-secretariat.edit', ['ctrlno'=>$trainingSecretariats->ctrlno]) }}" method="GET">
+                                @csrf
+                                <button class="mx-1 font-medium text-blue-600 hover:underline" type="submit">
+                                    <lord-icon
+                                        src="https://cdn.lordicon.com/bxxnzvfm.json"
+                                        trigger="hover"
+                                        colors="primary:#3a3347,secondary:#ffc738,tertiary:#f9c9c0,quaternary:#ebe6ef"
+                                        style="width:30px;height:30px">
+                                    </lord-icon>
+                                </button>
+                            </form>
+
+                            <form action="{{ route('training-secretariat.destroy', ['ctrlno'=>$trainingSecretariats->ctrlno]) }}" method="POST" id="delete_training_secretariat_form{{$trainingSecretariats->ctrlno}}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" id="deleteTrainingSecretariatButton{{$trainingSecretariats->ctrlno}}" onclick="openConfirmationDialog(this, 'Confirm Deletion', 'Are you sure you want to delete this info?')">
+                                    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                                    <lord-icon
+                                        src="https://cdn.lordicon.com/jmkrnisz.json"
+                                        trigger="hover"
+                                        colors="primary:#880808"
+                                        style="width:24px;height:24px">
+                                    </lord-icon>
+                                </button>
+                            </form> --}}
+                        </div>
+                    </td>
+                </tr>
+            {{-- @endforeach --}}
+        </tbody>
+    </table>
+</div>
+
+<div class="my-5">
+    {{ $trainingSecretariat->links() }}
+</div>
+
+@endsection
