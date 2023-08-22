d8f5ce653d07647133910c4aa0110f53027a496d
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-08-22 13:53:10 +0800

    table.blade.php for training secretariat

---
+++ resources/views/admin/competency/partials/training_type_library/training_secretariat/table.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+@section('title', 'Training Secretariat')
+@section('sub', 'Training Secretariat')
+@section('content')
+@include('admin.competency.view_profile.header')
+
+<div class="my-5 flex justify-end">
+    <a href="{{ route('training-category.recentlyDeleted') }}">
+        <lord-icon
+            src="https://cdn.lordicon.com/jmkrnisz.json"
+            trigger="hover"
+            colors="primary:#DC3545"
+            style="width:34px;height:34px">
+      </lord-icon>
+    </a>
+
+    <a href="{{ route('training-category.create') }}" class="btn btn-primary" >Add Training Secretariat</a>
+</div>
+
+<div class="table-management-training relative overflow-x-auto sm:rounded-lg shadow-lg">
+    <table class="w-full text-left text-sm text-gray-500">
+        <thead class="bg-blue-500 text-xs uppercase text-gray-700 text-white">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Ctrlno
+                </th>
+
+                <th scope="col" class="px-6 py-3">
+                    Description
+                </th>
+
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Action</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- @foreach ($trainingCategory as $trainingCategories) --}}
+                <tr class="border-b bg-white">
+                    <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
+                        {{-- {{ $trainingCategories->ctrlno }} --}}
+                    </td>
+
+                    <td class="px-6 py-3">
+                        {{-- {{ $trainingCategories->description }} --}}
+                    </td>
+
+                    <td class="px-6 py-4 text-right uppercase">
+                        <div class="flex">
+                            {{-- <form action="{{ route('training-category.edit', ['ctrlno'=>$trainingCategories->ctrlno]) }}" method="GET">
+                                @csrf
+                                <button class="mx-1 font-medium text-blue-600 hover:underline" type="submit">
+                                    <lord-icon
+                                        src="https://cdn.lordicon.com/bxxnzvfm.json"
+                                        trigger="hover"
+                                        colors="primary:#3a3347,secondary:#ffc738,tertiary:#f9c9c0,quaternary:#ebe6ef"
+                                        style="width:30px;height:30px">
+                                    </lord-icon>
+                                </button>
+                            </form>
+
+                            <form action="{{ route('training-category.destroy', ['ctrlno'=>$trainingCategories->ctrlno]) }}" method="POST" id="delete_training_category_form{{$trainingCategories->ctrlno}}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" id="deleteTrainingCategoryButton{{$trainingCategories->ctrlno}}" onclick="openConfirmationDialog(this, 'Confirm Deletion', 'Are you sure you want to delete this info?')">
+                                    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                                    <lord-icon
+                                        src="https://cdn.lordicon.com/jmkrnisz.json"
+                                        trigger="hover"
+                                        colors="primary:#880808"
+                                        style="width:24px;height:24px">
+                                    </lord-icon>
+                                </button>
+                            </form> --}}
+                        </div>
+                    </td>
+                </tr>
+            {{-- @endforeach --}}
+        </tbody>
+    </table>
+</div>
+
+{{-- <div class="my-5">
+    {{ $trainingCategory->links() }}
+</div> --}}
+
+@endsection
