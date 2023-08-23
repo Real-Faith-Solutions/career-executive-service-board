7f41e30bbee509b9bf760790092958f28ffba19c
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-08-23 11:25:16 +0800

    new encoder for ExaminationTakenController.php

--- app/Http/Controllers/ExaminationTakenController.php
+++ app/Http/Controllers/ExaminationTakenController.php
@@ -12,25 +12,23 @@ use Illuminate\Validation\Rule;
 class ExaminationTakenController extends Controller
 {
 
-    public function index($cesno){
-
+    public function index($cesno)
+    {
         $personalData = PersonalData::find($cesno);
         $examinationTaken = $personalData->examinationTakens;
 
         return view('admin.201_profiling.view_profile.partials.examinations_taken.table', compact('examinationTaken', 'cesno'));
-
     }
 
-    public function create($cesno){
-
+    public function create($cesno)
+    {
         $profileLibTblExamRef = ProfileLibTblExamRef::all();
 
         return view('admin.201_profiling.view_profile.partials.examinations_taken.form', compact('profileLibTblExamRef', 'cesno'));
-
     }
 
-    public function store(Request $request, $cesno){
-
+    public function store(Request $request, $cesno)
+    {
         $request->validate([
 
             'exam_code' => ['required', Rule::unique('profile_tblExaminations')->where('personal_data_cesno', $cesno)],
@@ -43,11 +41,9 @@ class ExaminationTakenController extends Controller
 
         ]);
 
-        $userFullName = Auth::user();
-        $userLastName = $userFullName ->last_name;
-        $userFirstName = $userFullName ->first_name;
-        $userMiddleName = $userFullName ->middle_name;
-        $userNameExtension = $userFullName ->name_extension;
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $encoder = $user->userName();
 
         $examinationTaken = new ExaminationsTaken([
 
@@ -58,7 +54,7 @@ class ExaminationTakenController extends Controller
             'license_number' => $request->license_number,
             'date_acquired' => $request->date_acquired,
             'date_validity' => $request->date_validity,
-            'encoder' => $userLastName." ".$userFirstName." ".$userMiddleName." ".$userNameExtension,
+            'encoder' => $encoder,
 
         ]);
 
@@ -67,21 +63,19 @@ class ExaminationTakenController extends Controller
         $examinationTakenPersonalDataId->examinationTakens()->save($examinationTaken);
 
         return to_route('examination-taken.index', ['cesno' => $cesno])->with('message', 'Successfuly Saved');
-
     }
 
-    public function edit($ctrlno, $cesno){
-
+    public function edit($ctrlno, $cesno)
+    {
         $profileLibTblExamRef = ProfileLibTblExamRef::all();
         $examinationTaken = ExaminationsTaken::find($ctrlno);
 
         return view('admin.201_profiling.view_profile.partials.examinations_taken.edit',
         compact('examinationTaken', 'profileLibTblExamRef', 'cesno'));
-
     }
 
-    public function update(Request $request, $ctrlno, $cesno){
-
+    public function update(Request $request, $ctrlno, $cesno)
+    {
         $request->validate([
 
             'exam_code' => ['required', Rule::unique('profile_tblExaminations')->where('personal_data_cesno', $cesno)->ignore($ctrlno, 'ctrlno')],
@@ -104,20 +98,18 @@ class ExaminationTakenController extends Controller
         $examinationTaken->save();
 
         return to_route('examination-taken.index', ['cesno' => $cesno])->with('message', 'Successfuly Saved');
-
     }
 
-    public function destroy($ctrlno){
-
+    public function destroy($ctrlno)
+    {
         $examinationTaken = ExaminationsTaken::find($ctrlno);
         $examinationTaken->delete();
 
         return redirect()->back()->with('message', 'Deleted Sucessfully');
-
     }
 
-    public function recentlyDeleted($cesno){
-
+    public function recentlyDeleted($cesno)
+    {
         //parent model
         $personalData = PersonalData::withTrashed()->find($cesno);
 
@@ -125,25 +117,21 @@ class ExaminationTakenController extends Controller
         $examinationTakensTrashedRecord = $personalData->examinationTakens()->onlyTrashed()->get();
 
         return view('admin.201_profiling.view_profile.partials.examinations_taken.trashbin', compact('examinationTakensTrashedRecord', 'cesno'));
-
     }
 
-    public function restore($ctrlno){
-
+    public function restore($ctrlno)
+    {
         $examinationTaken = ExaminationsTaken::withTrashed()->find($ctrlno);
         $examinationTaken->restore();
 
         return back()->with('message', 'Data Restored Sucessfully');
-
     }
 
-    public function forceDelete($ctrlno){
-
+    public function forceDelete($ctrlno)
+    {
         $examinationTaken = ExaminationsTaken::withTrashed()->find($ctrlno);
         $examinationTaken->forceDelete();
 
         return back()->with('message', 'Data Permanently Deleted');
-
     }
-
 }
