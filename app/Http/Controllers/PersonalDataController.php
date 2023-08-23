a97efec3a784df8ea4a61b98a9672ba48c7c95d9
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-08-23 11:45:02 +0800

    ProfileController.php

--- app/Http/Controllers/PersonalDataController.php
+++ app/Http/Controllers/PersonalDataController.php
@@ -7,12 +7,11 @@ use Illuminate\Http\Request;
 
 class PersonalDataController extends Controller
 {
-    public function show($cesno){
-
+    public function show($cesno)
+    {
         $mainProfile = PersonalData::find($cesno);
 
         return view('admin.201_profiling.view_profile.partials.personal_data.form', 
-        compact('mainProfile', 'cesno'));
-        
+        compact('mainProfile', 'cesno'));   
     }
 }
