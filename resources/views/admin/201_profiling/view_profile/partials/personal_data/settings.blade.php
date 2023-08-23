62a5ddfa328232fa80950128f4c1c59eeca85b5e
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-08-23 16:20:23 +0800

    ui settings modified

--- resources/views/admin/201_profiling/view_profile/partials/personal_data/settings.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/personal_data/settings.blade.php
@@ -12,35 +12,34 @@
                 <div class="w-full text-left text-gray-500">
                     <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
                         <h1 class="px-6 py-3 text-left">
-                            Credentials
+                            Change Password
                         </h1>
                     </div>
 
                     <div class="border-b bg-white px-6 py-3">
 
-                        <div class="mb-3">
-                            <label for="email">Email<sup>*</sup></label>
-                            <input id="email" name="email" readonly value="{{ $mainProfile->email }}" class="w-full px-3 py-2 border rounded">
+                        <div class="mb-3 flex items-center">
+                            <label for="email" class="w-1/4">Email<sup>*</sup></label>
+                            <input id="email" name="email" readonly value="{{ $mainProfile->email }}" class="w-3/4 px-3 py-2 border rounded">
                         </div>
 
-                        <div class="mb-3">
-                            <label for="password">Password<sup>*</sup></label>
-                            <div class="relative w-full">
-                                <input id="password" name="password" type="password" value="{{ $mainProfile->password }}" oninput="validateInput(password, 8, 'all'), checkPasswordMatch()" onkeypress="validateInput(password, 8, 'all'), checkPasswordMatch()" onblur="checkErrorMessage(password), checkPasswordMatch()" required class="w-full px-3 py-2 border rounded pr-10">
+                        <div class="mb-3 flex items-center">
+                            <label for="password" class="w-1/4">Password<sup>*</sup></label>
+                            <div class="relative w-3/4">
+                                <input id="password" name="password" type="password" oninput="validateInput(password, 8, 'all'), checkPasswordMatch()" onkeypress="validateInput(password, 8, 'all'), checkPasswordMatch()" onblur="checkErrorMessage(password), checkPasswordMatch()" required class="w-full px-3 py-2 border rounded pr-10">
                                 <i class="far fa-eye absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer toggle-password"></i>
                             </div>
-                            <p id="passwordError" class="input_error text-red-600"></p>
                         </div>
-
+                        <p id="passwordError" class="input_error text-red-600"></p>
                         
-                        <div class="mb-3">
-                            <label for="confirmPassword">Confirm Password<sup>*</sup></label>
-                            <div class="relative w-full">
-                                <input id="confirmPassword" name="confirmPassword" type="password" value="{{ $mainProfile->confirmPassword }}" oninput="validateInput(confirmPassword, 8, 'all'), checkPasswordMatch()" onkeypress="validateInput(confirmPassword, 8, 'all'), checkPasswordMatch()" onblur="checkErrorMessage(confirmPassword), checkPasswordMatch()" required class="w-full px-3 py-2 border rounded pr-10">
+                        <div class="mb-3 flex items-center">
+                            <label for="confirmPassword" class="w-1/4">Confirm Password<sup>*</sup></label>
+                            <div class="relative w-3/4">
+                                <input id="confirmPassword" name="confirmPassword" type="password" oninput="validateInput(confirmPassword, 8, 'all'), checkPasswordMatch()" onkeypress="validateInput(confirmPassword, 8, 'all'), checkPasswordMatch()" onblur="checkErrorMessage(confirmPassword), checkPasswordMatch()" required class="w-full px-3 py-2 border rounded pr-10">
                                 <i class="far fa-eye absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer toggle-confirm-password"></i>
                             </div>
-                            <p id="confirmPasswordError" class="input_error text-red-600"></p>
                         </div>
+                        <p id="confirmPasswordError" class="input_error text-red-600"></p>
 
                         <div class="flex justify-center mt-4">
                             <button class="btn btn-primary w-full md:w-auto" id="personal_data_save" type="submit">Save</button>
@@ -52,5 +51,6 @@
         </div>
     </form>
 </div>
+
 
 @endsection
