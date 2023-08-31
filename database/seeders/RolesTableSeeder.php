d2989d14a556b4a0e67c98d390710e366effcb21
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-08-31 11:56:47 +0800

    agency hr operator done

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -116,6 +116,28 @@ class RolesTableSeeder extends Seeder
             $cespes_operator->assignPermission($permission);
         }
 
+        $permissions_agency_hr_operator = [
+            'personal_data_edit', 'personal_data_view',
+            'family_profile_add', 'family_profile_edit', 'family_profile_view',
+            'educational_attainment_add', 'educational_attainment_edit', 'educational_attainment_view',
+            'examinations_taken_add', 'examinations_taken_edit', 'examinations_taken_view',
+            'scholarships_taken_add', 'scholarships_taken_edit', 'scholarships_taken_view',
+            'research_and_studies_add', 'research_and_studies_edit', 'research_and_studies_view',
+            'work_experience_add', 'work_experience_edit', 'work_experience_view',
+            'field_expertise_add', 'field_expertise_edit', 'field_expertise_view',
+            'ces_trainings_view', 'non_ces_trainings_add', 'non_ces_trainings_edit', 'non_ces_trainings_view',
+            'health_records_add', 'health_records_edit', 'health_records_view',
+            'awards_and_citations_add', 'awards_and_citations_edit', 'awards_and_citations_view',
+            'affiliations_add', 'affiliations_edit', 'affiliations_view',
+            'case_records_add', 'case_records_edit', 'case_records_view',
+            'language_dialects_add', 'language_dialects_edit', 'language_dialects_view',
+            'eligibility_rank_tracker_view', 'cespes_ratings_view', 'pdf_files_add', 'pdf_files_view',
+        ];
+        
+        foreach ($permissions_agency_hr_operator as $permission) {
+            $agency_hr_operator->assignPermission($permission);
+        }
+
         
         
     }
