1c8aa959714f19486b6cc6607c9911c45d51c1fa
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-01 11:46:32 +0800

    agency hr operator added

--- database/seeders/DefaultAccounts.php
+++ database/seeders/DefaultAccounts.php
@@ -244,6 +244,44 @@ class DefaultAccounts extends Seeder
         $user->assignRole('cespes_operator');
         // end seeding cespes operator
 
+        // seeding agency hr operator 
+        $agency_hr_operator = PersonalData::create([
+            'email' => 'agency_hr_operator@ces.com',
+            'status' => $faker->randomElement(['Active', 'Inactive', 'Retired', 'Deceased']),
+            'title' => $faker->randomElement(['Dr.', 'Mr.', 'Ms.', 'Atty.']),
+            'lastname' => $faker->lastName,
+            'firstname' => $faker->firstName,
+            'name_extension' => $faker->randomElement(['Sr.', 'Jr.', 'III']),
+            'middlename' => $faker->lastName,
+            'middleinitial' => 'r',
+            'nickname' => $faker->name,
+            'birth_date' => $faker->date,
+            'birth_place' => $faker->numberBetween(1, 5),
+            'gender' => $faker->randomElement(['Male', 'Female', 'Prefer Not to Say']),
+            'gender_by_choice' => $faker->randomElement(['Male', 'Female', 'Prefer Not to Say']),
+            'civil_status' => $faker->randomElement(['Single', 'Married', 'Divorce']),
+            'religion' => $faker->numberBetween(1, 5),
+            'height' => $faker->randomNumber(3),
+            'weight' => $faker->randomNumber(2),
+            'member_of_indigenous_group' => $faker->randomElement(['No', 'Yes']),
+            'single_parent' => $faker->randomElement(['No', 'Yes']),
+            'citizenship' => $faker->randomElement(['Filipino', 'Dual-Citizenship']),
+            'dual_citizenship' => $faker->country(),
+            'person_with_disability' => $faker->randomElement(['No', 'Yes']),
+        ]);
+
+        $user = $agency_hr_operator->users()->Create([
+            'email' => $agency_hr_operator->email,
+            'password' => Hash::make('12345'),
+            'is_active'		            => 'Active',
+            'last_updated_by'           => 'system encode',
+            'encoder'                   => 'system encode',
+            'default_password_change'   => 'true',
+        ]);
+
+        $user->assignRole('agency_hr_operator');
+        // end seeding agency hr operator
+
         // seeding users
         $personalDataRecords = PersonalData::where('email', '!=', 'admin@ces.com')->get();
 
