027e94d9929392ee9fe9cd14715668e81da94527
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-02 23:59:04 +0800

    roles table modified

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\PersonalData;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RolesController extends Controller
@@ -10,19 +11,9 @@ class RolesController extends Controller
     
     public function index(Request $request)
     {
-        $query = $request->input('search');
-        $sortBy = $request->input('sort_by', 'cesno'); // Default sorting by Ces No.
-        $sortOrder = $request->input('sort_order', 'asc'); // Default sorting order
+        $roles = Role::all();
 
-        $personalData = PersonalData::query()
-            ->where('lastname', "LIKE" ,"%$query%")
-            ->orWhere('firstname',  "LIKE","%$query%")
-            ->orWhere('middleinitial',  "LIKE","%$query%")
-            ->orWhere('name_extension',  "LIKE","%$query%")
-            ->orderBy($sortBy, $sortOrder)
-            ->paginate(25);
-
-        return view('admin\rights_management\roles', compact('personalData', 'query', 'sortBy', 'sortOrder'));
+        return view('admin\rights_management\roles', compact('roles'));
     }
 
 }
--- resources/views/admin/rights_management/roles.blade.php
+++ resources/views/admin/rights_management/roles.blade.php
@@ -1,74 +1,96 @@
 @extends('layouts.app')
-@section('title', '201 profiling table')
+@section('title', 'Language Dialect')
+@section('sub', 'Language Dialect')
 @section('content')
 
-    <section>
-        <div class="grid lg:grid-cols-3">
-            @include('components.search')
-        </div>
+<div class="my-5 flex justify-end">
+    <button class="btn btn-primary" id='add-edit-languages-btn'>Add Language Dialect</button>
+</div>
 
-        <div class="relative my-5 overflow-x-auto sm:rounded-lg">
-            <table class="w-full text-left text-sm text-gray-500">
-                <thead class="bg-gray-50 text-xs uppercase text-gray-700">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            <a href="{{ route('view-profile-201.index', ['sort_by' => 'cesno', 'sort_order' => $sortOrder === 'asc' ? 'desc' : 'asc', 'search' => $query]) }}" class="flex items-center space-x-1">
-                                Ces No.
-                                @if ($sortBy === 'cesno')
-                                    @if ($sortOrder === 'asc')
-                                        <svg class="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
-                                        </svg>
-                                    @else
-                                        <svg class="w-4 h-4 text-gray-500 transform rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
-                                        </svg>
-                                    @endif
-                                @endif
-                            </a>
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            <a href="{{ route('view-profile-201.index', ['sort_by' => 'lastname', 'sort_order' => $sortOrder === 'asc' ? 'desc' : 'asc', 'search' => $query]) }}" class="flex items-center space-x-1">
-                                Name
-                                @if ($sortBy === 'lastname')
-                                    @if ($sortOrder === 'asc')
-                                        <svg class="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
-                                        </svg>
-                                    @else
-                                        <svg class="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                                        </svg>
-                                    @endif
-                                @endif
-                            </a>
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            <span class="sr-only">Action</span>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                        @foreach ($personalData as $personalDatas)
-                            <tr class="border-b bg-white hover:bg-slate-400 hover:text-white">
-                                <th scope="col" class="px-6 py-3">
-                                    {{ $personalDatas->cesno }}
-                                </th>
-                                <td scope="col" class="px-6 py-3">
-                                    {{ $personalDatas->lastname }}, {{ $personalDatas->firstname }} {{ $personalDatas->middlename }}
-                                </td>
-                                <td class="px-6 py-4 text-right">
-                                    <a href="{{ route('personal-data.show', ['cesno' => $personalDatas->cesno]) }}" class="font-medium">View profile</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                </tbody>
-            </table>
-            <div class="my-5">
-                {{ $personalData->links() }}
+<div class="table-language-dialect relative overflow-x-auto sm:rounded-lg shadow-lg">
+    <table class="w-full text-left text-sm text-gray-500">
+        <thead class="bg-blue-500 text-xs uppercase text-gray-700 text-white">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Roles
+                </th>
+
+                <th scope="col" class="px-6 py-3">
+                    <span class="sr-only">Action</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($roles as $role)
+                <tr class="border-b bg-white">
+                    <td class="px-6 py-3">
+                       {{  $role->role_name }}
+                    </td>
+
+                    <td class="px-6 py-4 text-right uppercase">
+                        <div class="flex justify-end">
+                            {{-- <form action="{{ route('language.edit', ['ctrlno'=>$languages->ctrlno, 'cesno'=>$cesno]) }}" method="GET">
+                                @csrf
+                                <button class="mx-1 font-medium text-blue-600 hover:underline" type="submit">
+                                    <lord-icon
+                                        src="https://cdn.lordicon.com/bxxnzvfm.json"
+                                        trigger="hover"
+                                        colors="primary:#3a3347,secondary:#ffc738,tertiary:#f9c9c0,quaternary:#ebe6ef"
+                                        style="width:30px;height:30px">
+                                    </lord-icon>
+                                </button>
+                            </form>
+
+                            <form action="{{ route('language.destroy', ['ctrlno'=>$languages->ctrlno]) }}" method="POST" id="delete_language_form{{$languages->ctrlno}}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" id="deleteLanguageButton{{$languages->ctrlno}}" onclick="openConfirmationDialog(this, 'Confirm Deletion', 'Are you sure you want to delete this info?')">
+                                    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+                                    <lord-icon
+                                        src="https://cdn.lordicon.com/jmkrnisz.json"
+                                        trigger="hover"
+                                        colors="primary:#880808"
+                                        style="width:24px;height:24px">
+                                    </lord-icon>
+                                </button>
+                            </form> --}}
+                            <p>sample</p>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+</div>
+
+<!-- Modal for Adding Language Dialect -->
+<div id="add-edit-languages-modal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 hidden">
+    <div class="modal-content bg-white p-6 rounded-lg shadow-lg">
+        <form action="#" method="POST" class="flex flex-col items-center" id="new_role_form" onsubmit="return checkErrorsBeforeSubmit(new_role_form)">
+            @csrf
+
+            <span class="close-md absolute top-2 right-2 text-gray-600 cursor-pointer">&times;</span>
+            <h2 class="text-2xl font-bold mb-4 text-center">Add Language or Dialect</h2>
+
+            <div class="sm:gid-cols-1 mb-2 grid gap-4 md:grid-cols-2 lg:grid-cols-1">
+
+                <div class="mb-1">
+                    <label for="new_role">New Role<sup>*</sup></label>
+                    <input id="new_role" name="new_role" type="text" value="{{ old('new_role') }}" oninput="validateInput(new_role, 2, 'letters')" onkeypress="validateInput(new_role, 2, 'letters')" onblur="checkErrorMessage(new_role)" required>
+                    <p class="input_error text-red-600"></p>
+                    @error('new_role')
+                        <span class="invalid" role="alert">
+                            <p>{{ $message }}</p>
+                        </span>
+                    @enderror
+                </div>
+
             </div>
-        </div>
-
-    </section>
+            <button type="submit" id="addLanguagesBtn" class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">ADD</button>
+        </form>
+    </div>
+</div>
 
 @endsection
