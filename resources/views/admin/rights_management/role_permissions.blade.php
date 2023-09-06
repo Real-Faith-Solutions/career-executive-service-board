ec05c0c141fa074fbd5d2a195af52a1ba49fe869
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-06 15:48:13 +0800

    so far so good

--- resources/views/admin/rights_management/role_permissions.blade.php
+++ resources/views/admin/rights_management/role_permissions.blade.php
@@ -16,29 +16,62 @@
 </nav>
 
 
-<div class="relative overflow-x-auto sm:rounded-lg shadow-lg">
-    <table class="w-full text-left text-sm text-gray-500">
-        <thead class="bg-blue-500 text-xs uppercase text-gray-700 text-white">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Permissions
-                </th>
-            </tr>
-        </thead>
-        <tbody>
+{{-- <div class="relative overflow-x-auto sm:rounded-lg shadow-lg">
 
-            @foreach ($permissions as $permission)
+    <form method="POST" action="#">
+        @csrf
+        
+        <p>Personal Data</p>
+            <input type="checkbox" name="permissions[]" value="personal_data_add"
+            {{ $permissions->contains('permission_name', 'personal_data_add') ? 'checked' : '' }}>
+            Add
+        <button type="submit">Save Permissions</button>
+    </form>
 
-                <tr class="border-b bg-white hover:bg-slate-400 hover:text-white uppercase">
-                    <td class="px-6 py-3">
-                        {{ $permission->permission_name }}
-                    </td>
-                </tr>
-                
-            @endforeach
+</div> --}}
 
-        </tbody>
-    </table>
+<div class="grid lg:grid-cols-4 sm:grid-cols-1 gap-1">
+
+    <div class="col-span-1">
+        <div class="relative my-10 overflow-x-auto shadow-lg sm:rounded-lg">
+            <div class="w-full text-gray-500">
+                <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
+                    <h1 class="px-6 py-3">
+                        Personal Data
+                    </h1>
+                </div>
+    
+                <div class="border-b bg-white px-6 py-3">
+    
+                    <div class="sm:grid-cols-1 grid gap-2 md:grid-cols-1 lg:grid-cols-1">
+
+                        <div class="flex items-center mb-4">
+                            <input id="personal_data_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_add') ? 'checked' : '' }} value="personal_data_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                            <label for="personal_data_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                        </div>
+    
+                        <div class="flex items-center mb-4">
+                            <input id="personal_data_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_edit') ? 'checked' : '' }} value="personal_data_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                            <label for="personal_data_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                        </div>
+
+                        <div class="flex items-center mb-4">
+                            <input id="personal_data_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_delete') ? 'checked' : '' }} value="personal_data_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                            <label for="personal_data_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                        </div>
+
+                        <div class="flex items-center mb-4">
+                            <input id="personal_data_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_view') ? 'checked' : '' }} value="personal_data_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                            <label for="personal_data_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 @endsection
