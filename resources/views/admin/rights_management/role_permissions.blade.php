e6d7c2094c079772f3ee9ad6b41aaba8398cfdd5
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-07 17:01:27 +0800

    nice

--- resources/views/admin/rights_management/role_permissions.blade.php
+++ resources/views/admin/rights_management/role_permissions.blade.php
@@ -360,14 +360,14 @@
                 </div>
             </div>
     
-            {{-- educational attainment permissions --}}
+            {{-- CES Trainings permissions --}}
             <div class="col-span-1">
                 <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
                     <div class="w-full text-gray-500">
     
                         <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
                             <h1 class="px-6 py-3 text-xm sm:text-sm sm:py-3.5">
-                                Educational Attainment
+                                CES Trainings
                             </h1>
                         </div>
             
@@ -398,14 +398,14 @@
                 </div>
             </div>
     
-            {{-- examinations taken permissions --}}
+            {{-- Non-CES Training permissions --}}
             <div class="col-span-1">
                 <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
                     <div class="w-full text-gray-500">
     
                         <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
                             <h1 class="px-6 py-3">
-                                Examinations Taken
+                                Non-CES Training
                             </h1>
                         </div>
             
@@ -429,82 +429,6 @@
                             <div class="flex items-center mb-4">
                                 <input id="examinations_taken_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'examinations_taken_view') ? 'checked' : '' }} value="examinations_taken_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
                                 <label for="examinations_taken_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
-                            </div>
-    
-                        </div>
-                    </div>
-                </div>
-            </div>
-    
-            {{-- scholarships taken permissions --}}
-            <div class="col-span-1">
-                <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
-                    <div class="w-full text-gray-500">
-    
-                        <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
-                            <h1 class="px-6 py-3">
-                                Scholarships Taken
-                            </h1>
-                        </div>
-            
-                        <div class="border-b bg-white px-6 py-3">
-            
-                            <div class="flex items-center mb-4">
-                                <input id="scholarships_taken_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'scholarships_taken_add') ? 'checked' : '' }} value="scholarships_taken_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="scholarships_taken_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
-                            </div>
-    
-                            <div class="flex items-center mb-4">
-                                <input id="scholarships_taken_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'scholarships_taken_edit') ? 'checked' : '' }} value="scholarships_taken_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="scholarships_taken_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
-                            </div>
-    
-                            <div class="flex items-center mb-4">
-                                <input id="scholarships_taken_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'scholarships_taken_delete') ? 'checked' : '' }} value="scholarships_taken_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="scholarships_taken_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
-                            </div>
-    
-                            <div class="flex items-center mb-4">
-                                <input id="scholarships_taken_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'scholarships_taken_view') ? 'checked' : '' }} value="scholarships_taken_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="scholarships_taken_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
-                            </div>
-    
-                        </div>
-                    </div>
-                </div>
-            </div>
-    
-            {{-- research and studies permissions --}}
-            <div class="col-span-1">
-                <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
-                    <div class="w-full text-gray-500">
-    
-                        <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
-                            <h1 class="px-6 py-3">
-                                Research & Studies
-                            </h1>
-                        </div>
-            
-                        <div class="border-b bg-white px-6 py-3">
-            
-                            <div class="flex items-center mb-4">
-                                <input id="research_and_studies_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_add') ? 'checked' : '' }} value="research_and_studies_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
-                            </div>
-    
-                            <div class="flex items-center mb-4">
-                                <input id="research_and_studies_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_edit') ? 'checked' : '' }} value="research_and_studies_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
-                            </div>
-    
-                            <div class="flex items-center mb-4">
-                                <input id="research_and_studies_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_delete') ? 'checked' : '' }} value="research_and_studies_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
-                            </div>
-    
-                            <div class="flex items-center mb-4">
-                                <input id="research_and_studies_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_view') ? 'checked' : '' }} value="research_and_studies_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
                             </div>
     
                         </div>
