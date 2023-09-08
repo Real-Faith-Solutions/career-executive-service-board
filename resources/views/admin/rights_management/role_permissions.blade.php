cf23a89910d3cd2afd81ff374415fbb5f5c7ff34
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-08 11:15:44 +0800

    cespes ratings permissions added

--- resources/views/admin/rights_management/role_permissions.blade.php
+++ resources/views/admin/rights_management/role_permissions.blade.php
@@ -688,6 +688,44 @@
                     </div>
                 </div>
             </div>
+
+            {{-- cespes ratings permissions --}}
+            <div class="col-span-1">
+                <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
+                    <div class="w-full text-gray-500">
+    
+                        <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
+                            <h1 class="px-6 py-3">
+                                CESPES Ratings
+                            </h1>
+                        </div>
+            
+                        <div class="border-b bg-white px-6 py-3">
+            
+                            <div class="flex items-center mb-4">
+                                <input id="cespes_ratings_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'cespes_ratings_add') ? 'checked' : '' }} value="cespes_ratings_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="cespes_ratings_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="cespes_ratings_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'cespes_ratings_edit') ? 'checked' : '' }} value="cespes_ratings_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="cespes_ratings_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="cespes_ratings_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'cespes_ratings_delete') ? 'checked' : '' }} value="cespes_ratings_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="cespes_ratings_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="cespes_ratings_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'cespes_ratings_view') ? 'checked' : '' }} value="cespes_ratings_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="cespes_ratings_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                            </div>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
     
         </div>
     
