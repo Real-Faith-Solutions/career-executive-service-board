50a3bca3d0d80452873891b4c409e6db1bc1a204
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-09 22:33:04 +0800

    all goods

--- app/Http/Controllers/PermissionsController.php
+++ app/Http/Controllers/PermissionsController.php
@@ -8,24 +8,6 @@ use Illuminate\Http\Request;
 
 class PermissionsController extends Controller
 {
-    // public function updatePersonalEducationalPermissions(Request $request, $role_name, $role_title)
-    // {
-    //     $role = Role::where('role_name', $role_name)->first();
-    //     $permissions = $role->permissions;
-
-    //     $permissionsInThisForm = [
-    //         'personal_data_add', 'personal_data_edit', 'personal_data_delete', 'personal_data_view',
-    //         'family_profile_add', 'family_profile_edit', 'family_profile_delete', 'family_profile_view',
-    //         'educational_attainment_add', 'educational_attainment_edit', 'educational_attainment_delete', 'educational_attainment_view',
-    //         'examinations_taken_add', 'examinations_taken_edit', 'examinations_taken_delete', 'examinations_taken_view',
-    //         'scholarships_taken_add', 'scholarships_taken_edit', 'scholarships_taken_delete', 'scholarships_taken_view',
-    //         'research_and_studies_add', 'research_and_studies_edit', 'research_and_studies_delete', 'research_and_studies_view',
-    //     ];
-
-    //     $submittedPermissions = $request->input('permissions');
-
-    //     return redirect()->route('permissions.profiling', compact('role_name', 'role_title'))->with('info', 'Permissions Updated!');
-    // }
 
     public function updatePersonalEducationalPermissions(Request $request, $role_name, $role_title)
     {
@@ -46,14 +28,6 @@ class PermissionsController extends Controller
         // Get the submitted permissions
         $submittedPermissions = $request->input('permissions');
     
-        // Delete common permissions between $permissionsArray and $permissionsInThisForm
-        // $permissions = array_diff($permissions, $permissionsInThisForm);
-
-        // Add common permissions between $permissionsInThisForm and $submittedPermissions
-        // $submittedPermissions = array_intersect($permissionsInThisForm, $submittedPermissions);
-
-        // $permissions = array_merge($permissions, $submittedPermissions);
-
         // Remove permissions that are no longer needed
         $permissionsToRemove = array_diff($permissionsInThisForm, $submittedPermissions);
         foreach ($permissionsToRemove as $permissionName) {
