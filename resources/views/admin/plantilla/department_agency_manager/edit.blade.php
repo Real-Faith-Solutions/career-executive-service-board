a5497dffcc62fe7fe4eb982c1a437b642c6ed3e6
koykoy027 <villanuevajoshua27@gmail.com>
2023-09-13 16:28:29 +0800

    typo

--- resources/views/admin/plantilla/department_agency_manager/edit.blade.php
+++ resources/views/admin/plantilla/department_agency_manager/edit.blade.php
@@ -43,7 +43,7 @@
     <div class="relative my-10 overflow-x-auto shadow-lg sm:rounded-lg">
         <div class="w-full text-left text-gray-500">
             <div class="bg-blue-500 uppercase text-gray-700 text-white">
-                <h1>
+                <h1 class="px-6 py-3">
                     Department / Agency Manager
                 </h1>
             </div>
