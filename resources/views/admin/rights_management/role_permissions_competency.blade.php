a4379f7f4f50d27ecfb518fe4c546c7b09c5492f
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-16 14:09:37 +0800

    Competency Contacts permissions added

--- resources/views/admin/rights_management/role_permissions_competency.blade.php
+++ resources/views/admin/rights_management/role_permissions_competency.blade.php
@@ -38,30 +38,30 @@
     
                         <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
                             <h1 class="px-6 py-3">
-                                Personal Data
+                                Competency Contacts
                             </h1>
                         </div>
             
                         <div class="border-b bg-white px-6 py-3">
             
                             <div class="flex items-center mb-4">
-                                <input id="personal_data_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_add') ? 'checked' : '' }} value="personal_data_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="personal_data_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                                <input id="compentency_contacts_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_contacts_add') ? 'checked' : '' }} value="compentency_contacts_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_contacts_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="personal_data_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_edit') ? 'checked' : '' }} value="personal_data_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="personal_data_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                                <input id="compentency_contacts_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_contacts_edit') ? 'checked' : '' }} value="compentency_contacts_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_contacts_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="personal_data_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_delete') ? 'checked' : '' }} value="personal_data_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="personal_data_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                                <input id="compentency_contacts_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_contacts_delete') ? 'checked' : '' }} value="compentency_contacts_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_contacts_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="personal_data_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_view') ? 'checked' : '' }} value="personal_data_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="personal_data_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                                <input id="compentency_contacts_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_contacts_view') ? 'checked' : '' }} value="compentency_contacts_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_contacts_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
                             </div>
     
                         </div>
