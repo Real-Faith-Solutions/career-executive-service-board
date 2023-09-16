bb8ba04a5488c7b812411299876797ca1383288d
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-16 14:20:34 +0800

    Training Secretariat permissions added

--- resources/views/admin/rights_management/role_permissions_competency.blade.php
+++ resources/views/admin/rights_management/role_permissions_competency.blade.php
@@ -220,6 +220,44 @@
                     </div>
                 </div>
             </div>
+
+            {{-- reTraining Secretariat permissions --}}
+            <div class="col-span-1">
+                <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
+                    <div class="w-full text-gray-500">
+    
+                        <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
+                            <h1 class="px-6 py-3">
+                                Training Secretariat
+                            </h1>
+                        </div>
+            
+                        <div class="border-b bg-white px-6 py-3">
+            
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_training_secretariat_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_add') ? 'checked' : '' }} value="compentency_training_secretariat_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_training_secretariat_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_edit') ? 'checked' : '' }} value="compentency_training_secretariat_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_training_secretariat_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_delete') ? 'checked' : '' }} value="compentency_training_secretariat_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_training_secretariat_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_view') ? 'checked' : '' }} value="compentency_training_secretariat_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                            </div>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
     
             {{-- research and studies permissions --}}
             <div class="col-span-1">
@@ -235,23 +273,23 @@
                         <div class="border-b bg-white px-6 py-3">
             
                             <div class="flex items-center mb-4">
-                                <input id="research_and_studies_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_add') ? 'checked' : '' }} value="research_and_studies_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                                <input id="compentency_training_secretariat_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_add') ? 'checked' : '' }} value="compentency_training_secretariat_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="research_and_studies_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_edit') ? 'checked' : '' }} value="research_and_studies_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                                <input id="compentency_training_secretariat_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_edit') ? 'checked' : '' }} value="compentency_training_secretariat_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="research_and_studies_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_delete') ? 'checked' : '' }} value="research_and_studies_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                                <input id="compentency_training_secretariat_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_delete') ? 'checked' : '' }} value="compentency_training_secretariat_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="research_and_studies_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'research_and_studies_view') ? 'checked' : '' }} value="research_and_studies_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="research_and_studies_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                                <input id="compentency_training_secretariat_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_view') ? 'checked' : '' }} value="compentency_training_secretariat_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_training_secretariat_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
                             </div>
     
                         </div>
