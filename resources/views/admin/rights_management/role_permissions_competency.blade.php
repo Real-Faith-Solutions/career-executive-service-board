9355c9a5267b7d0038b7838941966777f2ffdac5
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-16 14:11:23 +0800

    Non-Ces Trainings permissions added

--- resources/views/admin/rights_management/role_permissions_competency.blade.php
+++ resources/views/admin/rights_management/role_permissions_competency.blade.php
@@ -31,7 +31,7 @@
     
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
     
-            {{-- personal data permissions --}}
+            {{-- Competency Contacts permissions --}}
             <div class="col-span-1">
                 <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
                     <div class="w-full text-gray-500">
@@ -76,30 +76,30 @@
                         
                         <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
                             <h1 class="px-6 py-3">
-                                Family Profile
+                                Non-Ces Trainings
                             </h1>
                         </div>
             
                         <div class="border-b bg-white px-6 py-3">
             
                             <div class="flex items-center mb-4">
-                                <input id="family_profile_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'family_profile_add') ? 'checked' : '' }} value="family_profile_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="family_profile_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                                <input id="compentency_non_ces_trainings_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_non_ces_trainings_add') ? 'checked' : '' }} value="compentency_non_ces_trainings_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_non_ces_trainings_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="family_profile_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'family_profile_edit') ? 'checked' : '' }} value="family_profile_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="family_profile_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                                <input id="compentency_non_ces_trainings_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_non_ces_trainings_edit') ? 'checked' : '' }} value="compentency_non_ces_trainings_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_non_ces_trainings_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="family_profile_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'family_profile_delete') ? 'checked' : '' }} value="family_profile_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="family_profile_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                                <input id="compentency_non_ces_trainings_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_non_ces_trainings_delete') ? 'checked' : '' }} value="compentency_non_ces_trainings_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_non_ces_trainings_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="family_profile_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'family_profile_view') ? 'checked' : '' }} value="family_profile_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="family_profile_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                                <input id="compentency_non_ces_trainings_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_non_ces_trainings_view') ? 'checked' : '' }} value="compentency_non_ces_trainings_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_non_ces_trainings_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
                             </div>
     
                         </div>
