9b58ff68e61659f8a2ab94a605053beb87c8b491
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-17 17:30:27 +0800

    Field Specialization permissions added

--- resources/views/admin/rights_management/role_permissions_competency.blade.php
+++ resources/views/admin/rights_management/role_permissions_competency.blade.php
@@ -221,7 +221,7 @@
                 </div>
             </div>
 
-            {{-- reTraining Secretariat permissions --}}
+            {{-- Training Secretariat permissions --}}
             <div class="col-span-1">
                 <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
                     <div class="w-full text-gray-500">
@@ -259,37 +259,75 @@
                 </div>
             </div>
     
-            {{-- research and studies permissions --}}
+            {{-- Field Specialization permissions --}}
             <div class="col-span-1">
                 <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
                     <div class="w-full text-gray-500">
     
                         <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
                             <h1 class="px-6 py-3">
-                                Research & Studies
+                                Field Specialization
                             </h1>
                         </div>
             
                         <div class="border-b bg-white px-6 py-3">
             
                             <div class="flex items-center mb-4">
-                                <input id="compentency_training_secretariat_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_add') ? 'checked' : '' }} value="compentency_training_secretariat_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="compentency_training_secretariat_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                                <input id="compentency_field_specialization_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_field_specialization_add') ? 'checked' : '' }} value="compentency_field_specialization_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_field_specialization_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="compentency_training_secretariat_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_edit') ? 'checked' : '' }} value="compentency_training_secretariat_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="compentency_training_secretariat_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                                <input id="compentency_field_specialization_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_field_specialization_edit') ? 'checked' : '' }} value="compentency_field_specialization_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_field_specialization_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="compentency_training_secretariat_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_delete') ? 'checked' : '' }} value="compentency_training_secretariat_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="compentency_training_secretariat_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                                <input id="compentency_field_specialization_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_field_specialization_delete') ? 'checked' : '' }} value="compentency_field_specialization_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_field_specialization_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
                             </div>
     
                             <div class="flex items-center mb-4">
-                                <input id="compentency_training_secretariat_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_training_secretariat_view') ? 'checked' : '' }} value="compentency_training_secretariat_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                                <label for="compentency_training_secretariat_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                                <input id="compentency_field_specialization_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_field_specialization_view') ? 'checked' : '' }} value="compentency_field_specialization_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_field_specialization_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                            </div>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- personal data permissions --}}
+            <div class="col-span-1">
+                <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
+                    <div class="w-full text-gray-500">
+    
+                        <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
+                            <h1 class="px-6 py-3">
+                                Personal Data
+                            </h1>
+                        </div>
+            
+                        <div class="border-b bg-white px-6 py-3">
+            
+                            <div class="flex items-center mb-4">
+                                <input id="personal_data_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_add') ? 'checked' : '' }} value="personal_data_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="personal_data_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="personal_data_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_edit') ? 'checked' : '' }} value="personal_data_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="personal_data_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="personal_data_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_delete') ? 'checked' : '' }} value="personal_data_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="personal_data_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="personal_data_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'personal_data_view') ? 'checked' : '' }} value="personal_data_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="personal_data_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
                             </div>
     
                         </div>
