59982ce4c155e376b12852ff7de7b0a165989666
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-17 17:35:32 +0800

    CES Trainings permissions added

--- resources/views/admin/rights_management/role_permissions_competency.blade.php
+++ resources/views/admin/rights_management/role_permissions_competency.blade.php
@@ -372,6 +372,44 @@
                     </div>
                 </div>
             </div>
+
+            {{-- CES Trainings permissions --}}
+            <div class="col-span-1">
+                <div class="relative my-2 overflow-x-auto shadow-lg sm:rounded-lg">
+                    <div class="w-full text-gray-500">
+    
+                        <div class="bg-blue-500 uppercase text-gray-700 text-white flex justify-between">
+                            <h1 class="px-6 py-3">
+                                CES Trainings
+                            </h1>
+                        </div>
+            
+                        <div class="border-b bg-white px-6 py-3">
+            
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_ces_training_add" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_ces_training_add') ? 'checked' : '' }} value="compentency_ces_training_add" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_ces_training_add" class="ml-2 mt-2 text-sm font-medium text-gray-900">Add</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_ces_training_edit" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_ces_training_edit') ? 'checked' : '' }} value="compentency_ces_training_edit" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_ces_training_edit" class="ml-2 mt-2 text-sm font-medium text-gray-900">Edit</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_ces_training_delete" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_ces_training_delete') ? 'checked' : '' }} value="compentency_ces_training_delete" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_ces_training_delete" class="ml-2 mt-2 text-sm font-medium text-gray-900">Delete</label>
+                            </div>
+    
+                            <div class="flex items-center mb-4">
+                                <input id="compentency_ces_training_view" type="checkbox" name="permissions[]" {{ $permissions->contains('permission_name', 'compentency_ces_training_view') ? 'checked' : '' }} value="compentency_ces_training_view" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+                                <label for="compentency_ces_training_view" class="ml-2 mt-2 text-sm font-medium text-gray-900">View</label>
+                            </div>
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
     
         </div>
     
