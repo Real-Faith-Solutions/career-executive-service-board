ed5b3695c80aa70558129f584e01df2956c28c7c
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-09-18 10:21:37 +0800

    training provider manager report pdf format

--- resources/views/admin/competency/reports/training_provider_report_pdf.blade.php
+++ resources/views/admin/competency/reports/training_provider_report_pdf.blade.php
@@ -1,101 +1,189 @@
-<style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Training Provider Manager Report</title>
 
-    td, th {
-        border: 1px solid black;
-        padding: 10px;
-        text-align: center;
-    }
+        <style>
+            @page {
+                margin-top: 100px;
+                /* margin-bottom: 100px; */
+                padding-bottom: 100px;
+            }
 
-    th {
-        background-color: #d3d3d3;
-    }
+            header{
+                position: fixed;
+                left: 0px;
+                right: 0px;
+                height: 60px;
+                margin-top: -60px;
+                text-align: center;
+            }
+            table {
+                border-collapse: collapse;
+                padding-left: 30px;
+                padding-right: 30px;
+                width: 100%;
+            }
+        
+            td, th {
+                text-align: center;
+            }
+        
+            td {
+                padding: 10px;
+                font-size: 13px;
+                text-align: left;
+            }
+        
+            th {
+                color: #284F87;
+                font-size: 15px;
+                text-transform: uppercase;
+                padding-bottom: 5px;
+                background-color: white;
+            }
+        
+            tr:nth-child(even) {
+                /* background-color: #6aabd1; */
+                background-color: #DCD6D0;
+            }
+        
+            .container {
+                text-align: center;
+            }
+        
+            .title {
+                margin-top: -20px;
+            }
+        
+            .title_name {
+                text-transform: uppercase;
+                font-size: 20px;
+                color: #284F87;
+            }
+        
+            .title_street {
+                margin-top: -20px;
+                font-size: 12px;
+            }
+        
+            .link {
+                margin-top: -7px;
+                font-size: 15px;
+            } 
+        
+            .thead {
+                font-size: 13px;
+            }
 
-    tr:nth-child(even) {
-        background-color: #b0d1e4;
-    }
+            .report_name {
+                text-transform: uppercase;
+                font-size: 16px;
+                color: #284F87;
+                margin-top: 30px;
+            }
+                
+            .page-break {
+                page-break-after: always;
+                margin-top: 200px;
+            }
+        </style>
+    </head>
 
-    tr:nth-child(odd) {
-        background-color: #6aabd1;
-    }
-</style>
+    <body>
+        <header>
+            <div class="container">
+                <div class="logo">
+                    <img src="{{ public_path("images/branding.png") }}" alt="" style="width: 100px; height: 100px;">
+                </div>
+            </div>
+            
+            <div class="title">
+                <p class="title_name">Career Executive Service Board</p>
+                <p class="title_street">No. 3 Marcelino St., Isidora Hills, Holy Spirit Drive, Diliman, Quezon City 1127</p>
+                <p class="link"><a href="www.cesboard.gov.ph" target="_blank">www.cesboard.gov.ph</a></p>
+                <p class="report_name">Training Provider Manager Report</p>
+            </div>
+        </header>
 
-<div>
-    <table>
-        <thead>
-            <tr>
-                <th>
-                    Provider
-                </th>
-
-                <th>
-                    House Building
-                </th>
-
-                <th>
-                    St. Road
-                </th>
-
-                <th>
-                    Barangay/Village
-                </th>
-
-                <th>
-                    City Code
-                </th>
-
-                <th>
-                    Contact No.
-                </th>
-
-                <th>
-                    Email
-                </th>
-
-                <th>
-                    Contact Person
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($competencyTrainingProvider as $competencyTrainingProviders)
-                <tr>
-                    <td>
-                        {{ $competencyTrainingProviders->provider }}
-                    </td>
-
-                    <td>
-                        {{ $competencyTrainingProviders->house_bldg }}
-                    </td>
-
-                    <td>
-                        {{ $competencyTrainingProviders->st_road }}
-                    </td>
-
-                    <td>
-                        {{ $competencyTrainingProviders->brgy_vill }}
-                    </td>
-
-                    <td>
-                        {{ $competencyTrainingProviders->trainingProviderManager->name }}
-                    </td>
-
-                    <td>
-                        {{ $competencyTrainingProviders->contactno }}
-                    </td>
-
-                    <td>
-                        {{ $competencyTrainingProviders->emailadd }}
-                    </td>
-
-                    <td>
-                        {{ $competencyTrainingProviders->contactperson }}
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+        <div>
+            <table>
+                <thead>
+                    <div class="page-break"></div>
+                    <tr>
+                        <th>
+                            Provider
+                        </th>
+        
+                        <th>
+                            House Building
+                        </th>
+        
+                        <th>
+                            St. Road
+                        </th>
+        
+                        <th>
+                            Barangay
+                        </th>
+        
+                        <th>
+                            City Code
+                        </th>
+        
+                        <th>
+                            Contact No.
+                        </th>
+        
+                        <th>
+                            Email
+                        </th>
+        
+                        <th>
+                            Contact Person
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($competencyTrainingProvider as $competencyTrainingProviders)
+                        <tr>
+                            <td>
+                                {{ $competencyTrainingProviders->provider }}
+                            </td>
+        
+                            <td>
+                                {{ $competencyTrainingProviders->house_bldg }}
+                            </td>
+        
+                            <td>
+                                {{ $competencyTrainingProviders->st_road }}
+                            </td>
+        
+                            <td>
+                                {{ $competencyTrainingProviders->brgy_vill }}
+                            </td>
+        
+                            <td>
+                                {{ $competencyTrainingProviders->trainingProviderManager->name }}
+                            </td>
+        
+                            <td>
+                                {{ $competencyTrainingProviders->contactno }}
+                            </td>
+        
+                            <td>
+                                {{ $competencyTrainingProviders->emailadd }}
+                            </td>
+        
+                            <td>
+                                {{ $competencyTrainingProviders->contactperson }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>    
+    </body>
+</html>
