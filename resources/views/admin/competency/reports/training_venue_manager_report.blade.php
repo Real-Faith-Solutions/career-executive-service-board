56c7d037ebed6e9ece122acc149e4d0d8dd5be56
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-09-18 16:43:24 +0800

    filter data and new route for competency route

--- resources/views/admin/competency/reports/training_venue_manager_report.blade.php
+++ resources/views/admin/competency/reports/training_venue_manager_report.blade.php
@@ -3,37 +3,33 @@
 @section('sub', 'Training Venue Manager')
 @section('content')
 
-    <h1 class="uppercase font-semibold text-blue-600 text-2xl text-center">Training Venue Manager Report</h1>
+    <h1 class="uppercase font-semibold text-blue-600 text-2xl">Training Venue Manager Report</h1>
 
     <div class="my-5 flex justify-between">
         {{-- search bar --}}
             <div class="flex items-center">
-                <form>
-                    <div class="w-100">
-                        <label for="default-search" class="sr-only mb-2 text-sm font-medium text-gray-900">Search</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <button type="submit">
-                                        <svg aria-hidden="true" class="h-5 w-5 text-gray-500" fill="none" stroke="currentColor"
-                                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                        </svg>
-                                    </button>
-                            </div>
-                            <input type="search" id="default-search" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" placeholder="Search here..." name="search" @if (!empty($search)) value="{{ $search }}" @endif autofocus autocomplete="search">
-                        </div>
-                    </div>
+                <form class="flex gap-3">
+                    <select id="city_code" name="city_code">
+                        <option value="">Select All</option>
+                        @foreach($profileLibTblCities as $profileLibTblCity)
+                            <option value="{{ $profileLibTblCity->city_code }}" {{ $cityCode == $profileLibTblCity->city_code ? 'selected' : '' }}>
+                                {{ $profileLibTblCity->name  }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <button class="btn btn-primary" type="submit">Filter</button>
                 </form>
             </div>
         {{-- end of search bar --}}
 
-        <form action="{{ route('competency-management-sub-modules-report.trainingVenueManagerReportGeneratePdf') }}" target="_blank" method="POST">
-            @csrf
-            <button class="btn btn-primary mx-1 font-medium text-blue-600" type="submit">
-                Generate PDF Report
-            </button>
-        </form>
+        <div class="flex">
+            <form action="{{ route('competency-management-sub-modules-report.trainingVenueManagerReportGeneratePdf') }}" target="_blank" method="POST">
+                @csrf
+                <button class="btn btn-primary mx-1 font-medium text-blue-600" type="submit">
+                    Generate PDF Report
+                </button>
+            </form>
+        </div>
     </div>
 
     <div class="table-management-training relative overflow-x-auto sm:rounded-lg shadow-lg">
