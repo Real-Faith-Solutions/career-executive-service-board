97defa1459a23e713260936f96c7e68efc86650b
koykoy027 <villanuevajoshua27@gmail.com>
2023-09-20 15:31:02 +0800

    change lastupd_date into lastupd_dt

--- resources/views/admin/plantilla/appointee_occupant_manager/edit.blade.php
+++ resources/views/admin/plantilla/appointee_occupant_manager/edit.blade.php
@@ -387,7 +387,7 @@
 
                     <div class="flex justify-between">
                         <h1 class="text-slate-400 text-sm font-semibold">
-                            Last update at {{ \Carbon\Carbon::parse($planPosition->lastupd_date)->format('m/d/Y \a\t
+                            Last update at {{ \Carbon\Carbon::parse($planPosition->lastupd_dt)->format('m/d/Y \a\t
                             g:iA') }}
                         </h1>
                         <button type="submit" class="btn btn-primary">
