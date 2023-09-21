41a19f7cefadaa9044cc94df3dceccd4b32f184f
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-09-21 13:01:17 +0800

    device_verification migration added

---
+++ database/migrations/2023_09_21_105426_create_device_verification_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('device_verification', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('personal_data_cesno');
+            $table->foreign('personal_data_cesno')->references('cesno')->on('profile_tblMain')->onDelete('cascade');
+            $table->string('confirmation_code');
+            $table->string('device_id')->unique();
+            $table->boolean('verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('device_verification');
+    }
+};
