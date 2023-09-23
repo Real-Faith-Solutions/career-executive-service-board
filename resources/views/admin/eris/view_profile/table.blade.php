a533088b99ac32192f8a3cc4a099cd968c1cd0d5
Jon Rolando D. Manuel <manueljonrolando@gmail.com>
2023-09-23 16:43:25 +0800

    eris routing conflict solved (#335)

--- resources/views/admin/eris/view_profile/table.blade.php
+++ resources/views/admin/eris/view_profile/table.blade.php
@@ -3,8 +3,15 @@
 @section('content')
 
     <section>
-        <div class="grid lg:grid-cols-3">
-            @include('components.search')
+    
+        <div class="flex justify-between">
+            <div class="grid lg:grid-cols-3">
+                @include('components.search')
+            </div>
+
+            <div class="mt-5">
+                <a href="" class="btn btn-primary" >Add New Profile</a>
+            </div>
         </div>
 
         <div class="relative my-5 overflow-x-auto sm:rounded-lg">
