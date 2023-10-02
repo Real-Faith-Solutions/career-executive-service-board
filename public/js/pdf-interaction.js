d822abe981b0cf6c9d2c911ee9c12aeb5901b8fb
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-10-02 15:16:41 +0800

    pdf interaction js modified

--- public/js/pdf-interaction.js
+++ public/js/pdf-interaction.js
@@ -1,4 +1,4 @@
-  // Show the modal
+  // Show the approve modal
   function openConfirmationDialogApprovePendingPdf(ctrlno, personal_data_cesno) {
     const confirmationDialogApprovePendingPdf = document.getElementById('approve_pending_pdf_modal');
     const approve_file_ctrlno = document.getElementById('approve_file_ctrlno');
@@ -10,19 +10,48 @@
     confirmationDialogApprovePendingPdf.classList.remove('hidden');
   }
   
-  // Close the modal when the close button is clicked
+  // Close the approve modal when the close button is clicked
   document.getElementsByClassName('close-md')[0].addEventListener('click', function() {
     closeConfirmationDialogApprovePendingPdf();
   });
   
-  // Close the modal when the user clicks outside the modal content
+  // Close the approve modal when the user clicks outside the modal content
   window.addEventListener('click', function(event) {
     if (event.target == document.getElementById('approve_pending_pdf_modal')) {
       closeConfirmationDialogApprovePendingPdf();
     }
   });
   
-  // Close the modal
+  // Close the approve modal
   function closeConfirmationDialogApprovePendingPdf() {
     document.getElementById('approve_pending_pdf_modal').classList.add('hidden');
+  }
+  
+  // Show the decline modal
+  function openConfirmationDialogDeclinePendingPdf(ctrlno, personal_data_cesno) {
+    const ConfirmationDialogDeclinePendingPdf = document.getElementById('decline_pending_pdf_modal');
+    const decline_file_ctrlno = document.getElementById('decline_file_ctrlno');
+    const decline_file_personal_data_cesno = document.getElementById('decline_file_personal_data_cesno');
+
+    decline_file_ctrlno.value = ctrlno;
+    decline_file_personal_data_cesno.value = personal_data_cesno;
+    
+    ConfirmationDialogDeclinePendingPdf.classList.remove('hidden');
+  }
+  
+  // Close the decline modal when the close button is clicked
+  document.getElementsByClassName('close-md')[0].addEventListener('click', function() {
+    closeConfirmationDialogDeclinePendingPdf();
+  });
+  
+  // Close the decline modal when the user clicks outside the modal content
+  window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('decline_pending_pdf_modal')) {
+      closeConfirmationDialogDeclinePendingPdf();
+    }
+  });
+  
+  // Close the decline modal
+  function closeConfirmationDialogDeclinePendingPdf() {
+    document.getElementById('decline_pending_pdf_modal').classList.add('hidden');
   }
