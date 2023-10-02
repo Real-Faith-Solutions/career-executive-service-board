eaf5f69f2efea3930902378d96806a0fe3b83346
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-10-02 15:19:35 +0800

    routes and action added declinePendingPdfForm

--- resources/views/admin/201_profiling/view_profile/partials/pdf_files/pendingFileTable.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/pdf_files/pendingFileTable.blade.php
@@ -152,7 +152,7 @@
 <div id="decline_pending_pdf_modal"
     class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 hidden">
     <div class="modal-content bg-white p-6 rounded-lg shadow-lg">
-        <form id="declinePendingPdfForm" action="{{ route('show-pdf-files.acceptedFiles') }}"
+        <form id="declinePendingPdfForm" action="{{ route('declineFile') }}"
             method="POST" class="flex flex-col items-center"
             onsubmit="return checkErrorsBeforeSubmit(declinePendingPdfForm)">
             @csrf
