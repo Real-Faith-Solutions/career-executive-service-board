698ae456c5048b11a146d6246513c6a1e190a5db
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-10-06 14:13:26 +0800

    training provider manager all related to migration changed

--- database/migrations/trainings/2023_06_06_100341_create_training_tblProvider_table.php
+++ database/migrations/trainings/2023_06_06_100341_create_training_tblProvider_table.php
@@ -13,18 +13,19 @@ return new class extends Migration
     {
         Schema::create('training_tblProvider', function (Blueprint $table) {
             $table->id('providerID');
-            $table->string('provider');
+            $table->string('provider')->nullable();
             $table->string('house_bldg')->nullable();
             $table->string('st_road')->nullable();
             $table->string('brgy_vill')->nullable();
-            $table->foreignId('city_code')->constrained('profilelib_tblcities', 'city_code');
+            $table->integer('city_code')->nullable();
+            // $table->foreignId('city_code')->constrained('profilelib_tblcities', 'city_code');
             $table->string('contactno')->nullable();
             $table->string('emailadd')->nullable();
             $table->string('contactperson')->nullable();
             $table->string('encoder')->nullable();
             $table->string('lastupd_enc')->nullable();
-            $table->timestamp('encdate')->nullable()->useCurrent();
-            $table->timestamp('lastupd_dt')->nullable()->useCurrent();
+            $table->timestamp('encdate')->nullable()->useCurrent()->nullable();
+            $table->timestamp('lastupd_dt')->nullable()->useCurrent()->nullable();
             $table->softDeletes();
         });
     }
