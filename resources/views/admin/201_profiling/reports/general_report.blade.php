196c9d96477b9c905a6856b2f4dd2e11c7a7d8e3
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-10-07 14:20:25 +0800

    ces status filter modified

--- resources/views/admin/201_profiling/reports/general_report.blade.php
+++ resources/views/admin/201_profiling/reports/general_report.blade.php
@@ -61,8 +61,8 @@
                 </div>
 
                 <div class="flex items-center px-6 py-3 text-left">
-                    <label for="cesstat_code">CES Status<sup>*</sup></label>
-                    <select id="cesstat_code" name="cesstat_code" required type="text">
+                    <label for="cesstat_code" class="mr-2 mt-2 text-sm font-medium text-gray-700">CES Status<sup>*</sup></label>
+                    <select id="cesstat_code" name="cesstat_code" required type="text" class="inline-block">
                         <option disabled selected>Select CES Status</option>
                         @foreach ($profileLibTblCesStatus as $newProfileLibTblCesStatus)
                             <option value="{{ $newProfileLibTblCesStatus->code }}">{{ $newProfileLibTblCesStatus->description }}</option>
