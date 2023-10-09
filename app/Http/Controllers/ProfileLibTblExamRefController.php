03f66ea11bbe263786ffd210474e72437a8ca159
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-10-09 10:42:38 +0800

    index method and destroy method for examination library

---
+++ app/Http/Controllers/ProfileLibTblExamRefController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProfileLibTblExamRef;
+use Illuminate\Http\Request;
+
+class ProfileLibTblExamRefController extends Controller
+{
+    public function index()
+    {
+        $profileLibTblExamRef = ProfileLibTblExamRef::select('CODE' ,'TITLE')->paginate(25);
+
+        return view('admin.201_library.examination.index', compact('profileLibTblExamRef'));
+    }
+
+    public function destroy($code)
+    {
+        $profileLibTblExamRef = ProfileLibTblExamRef::find($code);
+        $profileLibTblExamRef->delete();
+
+        return back()->with('message', 'Deleted Sucessfully');
+    }
+}
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -426,6 +426,11 @@
                             class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                             Educational Degree
                         </a>
+
+                        <a href="{{ route('examination.index') }}"
+                            class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                            Examination
+                        </a>
                     </li>
 
                 </ul>
