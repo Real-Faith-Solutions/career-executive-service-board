fc60dd5b0c967c27ae574f658b517bfb287a2fda
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-10-10 17:34:52 +0800

    ?? 'No Record' added to training session and chaning date format to m/d/y

--- app/Http/Controllers/TrainingSessionController.php
+++ app/Http/Controllers/TrainingSessionController.php
@@ -17,164 +17,162 @@ use Illuminate\Validation\Rule;
 
 class TrainingSessionController extends Controller
 {
-    // training session methods
-        public function index()
-        {
-            $trainingSession = TrainingSession::paginate(25);
+    public function index()
+    {
+        $trainingSession = TrainingSession::paginate(25);
 
-            return view('admin.competency.partials.training_session.table', compact('trainingSession'));
+        return view('admin.competency.partials.training_session.table', compact('trainingSession'));
+    }
+
+    public function create()
+    {
+        $trainingLibCategory = TrainingLibCategory::all();
+        $competencyTrainingVenueManager = CompetencyTrainingVenueManager::all();
+        $trainingSecretariat = TrainingSecretariat::all();
+        $profileLibTblExpertiseGen = ProfileLibTblExpertiseGen::all();
+        $resourceSpeaker = ResourceSpeaker::all();
+
+        return view('admin.competency.partials.training_session.form', compact('trainingLibCategory', 'competencyTrainingVenueManager', 'trainingSecretariat', 'profileLibTblExpertiseGen', 'resourceSpeaker'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+     'title' => ['required', 'max:60', 'min:2', 'regex:/^[a-zA-Z ]*$/', 'unique:training_tblSessions,title'],
+     'category' => ['required'],
+     'specialization' => ['required'],
+     'from_dt' => ['required'],
+     'to_dt' => ['required'],
+     'venue' => ['required'],
+     'no_hours' => ['required', 'numeric', 'digits_between:1,4'],
+     'barrio' => ['nullable', 'max:60', 'min:2'],
+     'resource_speaker' => ['required'],
+     'session_director' => ['required'],
+     'status' => ['required'],
+     'remarks' => ['required', 'regex:/^[a-zA-Z ]*$/'],
+        ]);
+
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $encoder = $user->userName();
+
+        TrainingSession::create([
+     'title' => $request->title,
+     'category' => $request->category,  
+     'specialization' => $request->specialization,  
+     'from_dt' => $request->from_dt,  
+     'to_dt' => $request->to_dt,  
+     'venueId' => $request->venue,  
+     'status' => $request->status,  
+     'remarks' => $request->remarks,  
+     'barrio' => $request->barrio,  
+     'no_hours' => $request->no_hours,  
+     'session_director' => $request->session_director,   
+     'speakerid' => $request->resource_speaker,  
+     'encoder' => $encoder,    
+        ]);
+
+        return to_route('training-session.index')->with('message', 'Save Sucessfully');
+    }
+
+    public function edit($ctrlno)
+    {
+        $trainingSession = TrainingSession::find($ctrlno);
+
+        if(!$trainingSession)
+        {
+     return redirect()->back()->with('error', 'Something Went Wrong');
         }
 
-        public function create()
-        {
-            $trainingLibCategory = TrainingLibCategory::all();
-            $competencyTrainingVenueManager = CompetencyTrainingVenueManager::all();
-            $trainingSecretariat = TrainingSecretariat::all();
-            $profileLibTblExpertiseGen = ProfileLibTblExpertiseGen::all();
-            $resourceSpeaker = ResourceSpeaker::all();
+        $trainingLibCategory = TrainingLibCategory::all();
+        $competencyTrainingVenueManager = CompetencyTrainingVenueManager::all();
+        $trainingSecretariat = TrainingSecretariat::all();
+        $profileLibTblExpertiseGen = ProfileLibTblExpertiseGen::all();
+        $resourceSpeaker = ResourceSpeaker::all();
 
-            return view('admin.competency.partials.training_session.form', compact('trainingLibCategory', 'competencyTrainingVenueManager', 'trainingSecretariat', 'profileLibTblExpertiseGen', 'resourceSpeaker'));
+        return view('admin.competency.partials.training_session.edit', compact('trainingSession', 'trainingLibCategory', 'competencyTrainingVenueManager', 'trainingSecretariat', 'profileLibTblExpertiseGen', 'resourceSpeaker'));
+    }
+
+    public function update(Request $request, $ctrlno)
+    {
+        $request->validate([
+     'title' => ['required', 'max:60', 'min:2', 'regex:/^[a-zA-Z ]*$/', Rule::unique('training_tblSessions')->ignore($ctrlno, 'sessionid')],
+     'category' => ['required'],
+     'specialization' => ['required'],
+     'from_dt' => ['required'],
+     'to_dt' => ['required'],
+     'venue' => ['required'],
+     'no_hours' => ['required','numeric', 'digits_between:1,4'],
+     'barrio' => ['nullable', 'max:60', 'min:2'],
+     'resource_speaker' => ['required'],
+     'session_director' => ['required'],
+     'status' => ['required'],
+     'remarks' => ['required', 'regex:/^[a-zA-Z ]*$/'],
+        ]);
+        
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $encoder = $user->userName();
+
+        $trainingSession = TrainingSession::find($ctrlno);
+        $trainingSession->title = $request->title;
+        $trainingSession->category = $request->category;
+        $trainingSession->specialization = $request->specialization;
+        $trainingSession->from_dt = $request->from_dt;
+        $trainingSession->to_dt = $request->to_dt;
+        $trainingSession->venueid = $request->venue;
+        $trainingSession->status = $request->status;
+        $trainingSession->remarks = $request->remarks;
+        $trainingSession->barrio = $request->barrio;
+        $trainingSession->no_hours = $request->no_hours;
+        $trainingSession->session_director = $request->session_director;
+        $trainingSession->speakerid = $request->resource_speaker;
+        $trainingSession->lastupd_enc = $encoder;
+        $trainingSession->update();
+
+        return to_route('training-session.index')->with('message', 'Update Sucessfully');
+    }
+
+    public function destroy($ctrlno)
+    {
+        $trainingSession = TrainingSession::find($ctrlno);
+
+        // count the participant that already register to training session
+        $trainingParticipantList = $trainingSession->trainingParticipantList()->withTrashed()->count();
+
+        $participantCount = 1;
+
+        if($trainingParticipantList >= $participantCount)
+        {
+     return redirect()->back()->with('error', 'The training session already has participants, so it cannot be deleted !!');
         }
 
-        public function store(Request $request)
-        {
-            $request->validate([
-                'title' => ['required', 'max:60', 'min:2', 'regex:/^[a-zA-Z ]*$/', 'unique:training_tblSessions,title'],
-                'category' => ['required'],
-                'specialization' => ['required'],
-                'from_dt' => ['required'],
-                'to_dt' => ['required'],
-                'venue' => ['required'],
-                'no_hours' => ['required', 'numeric', 'digits_between:1,4'],
-                'barrio' => ['nullable', 'max:60', 'min:2'],
-                'resource_speaker' => ['required'],
-                'session_director' => ['required'],
-                'status' => ['required'],
-                'remarks' => ['required', 'regex:/^[a-zA-Z ]*$/'],
-            ]);
+        $trainingSession->delete();
 
-            /** @var \App\Models\User $user */
-            $user = Auth::user();
-            $encoder = $user->userName();
+        return back()->with('message', 'Deleted Sucessfully');
+    }
 
-            TrainingSession::create([
-                'title' => $request->title,
-                'category' => $request->category,  
-                'specialization' => $request->specialization,  
-                'from_dt' => $request->from_dt,  
-                'to_dt' => $request->to_dt,  
-                'venueId' => $request->venue,  
-                'status' => $request->status,  
-                'remarks' => $request->remarks,  
-                'barrio' => $request->barrio,  
-                'no_hours' => $request->no_hours,  
-                'session_director' => $request->session_director,   
-                'speakerid' => $request->resource_speaker,  
-                'encoder' => $encoder,    
-            ]);
+    public function recentlyDeleted()
+    {
+        $trainingSessionTrashedRecord = TrainingSession::onlyTrashed()->paginate(25);
 
-            return to_route('training-session.index')->with('message', 'Save Sucessfully');
-        }
-
-        public function edit($ctrlno)
-        {
-            $trainingSession = TrainingSession::find($ctrlno);
-
-            if(!$trainingSession)
-            {
-                return redirect()->back()->with('error', 'Something Went Wrong');
-            }
-
-            $trainingLibCategory = TrainingLibCategory::all();
-            $competencyTrainingVenueManager = CompetencyTrainingVenueManager::all();
-            $trainingSecretariat = TrainingSecretariat::all();
-            $profileLibTblExpertiseGen = ProfileLibTblExpertiseGen::all();
-            $resourceSpeaker = ResourceSpeaker::all();
-
-            return view('admin.competency.partials.training_session.edit', compact('trainingSession', 'trainingLibCategory', 'competencyTrainingVenueManager', 'trainingSecretariat', 'profileLibTblExpertiseGen', 'resourceSpeaker'));
-        }
-
-        public function update(Request $request, $ctrlno)
-        {
-            $request->validate([
-                'title' => ['required', 'max:60', 'min:2', 'regex:/^[a-zA-Z ]*$/', Rule::unique('training_tblSessions')->ignore($ctrlno, 'sessionid')],
-                'category' => ['required'],
-                'specialization' => ['required'],
-                'from_dt' => ['required'],
-                'to_dt' => ['required'],
-                'venue' => ['required'],
-                'no_hours' => ['required','numeric', 'digits_between:1,4'],
-                'barrio' => ['nullable', 'max:60', 'min:2'],
-                'resource_speaker' => ['required'],
-                'session_director' => ['required'],
-                'status' => ['required'],
-                'remarks' => ['required', 'regex:/^[a-zA-Z ]*$/'],
-            ]);
-            
-            /** @var \App\Models\User $user */
-            $user = Auth::user();
-            $encoder = $user->userName();
-
-            $trainingSession = TrainingSession::find($ctrlno);
-            $trainingSession->title = $request->title;
-            $trainingSession->category = $request->category;
-            $trainingSession->specialization = $request->specialization;
-            $trainingSession->from_dt = $request->from_dt;
-            $trainingSession->to_dt = $request->to_dt;
-            $trainingSession->venueid = $request->venue;
-            $trainingSession->status = $request->status;
-            $trainingSession->remarks = $request->remarks;
-            $trainingSession->barrio = $request->barrio;
-            $trainingSession->no_hours = $request->no_hours;
-            $trainingSession->session_director = $request->session_director;
-            $trainingSession->speakerid = $request->resource_speaker;
-            $trainingSession->lastupd_enc = $encoder;
-            $trainingSession->update();
-
-            return to_route('training-session.index')->with('message', 'Update Sucessfully');
-        }
-
-        public function destroy($ctrlno)
-        {
-            $trainingSession = TrainingSession::find($ctrlno);
-
-            // count the participant that already register to training session
-            $trainingParticipantList = $trainingSession->trainingParticipantList()->withTrashed()->count();
-
-            $participantCount = 1;
-
-            if($trainingParticipantList >= $participantCount)
-            {
-                return redirect()->back()->with('error', 'The training session already has participants, so it cannot be deleted !!');
-            }
-
-            $trainingSession->delete();
-
-            return back()->with('message', 'Deleted Sucessfully');
-        }
-
-        public function recentlyDeleted()
-        {
-            $trainingSessionTrashedRecord = TrainingSession::onlyTrashed()->paginate(25);
-
-            return view('admin.competency.partials.training_session.trashbin', compact('trainingSessionTrashedRecord'));
-        }
+        return view('admin.competency.partials.training_session.trashbin', compact('trainingSessionTrashedRecord'));
+    }
 
 
-        public function restore($ctrlno)
-        {
-            $trainingSessionTrashedRecord = TrainingSession::onlyTrashed()->find($ctrlno);
-            $trainingSessionTrashedRecord->restore();
+    public function restore($ctrlno)
+    {
+        $trainingSessionTrashedRecord = TrainingSession::onlyTrashed()->find($ctrlno);
+        $trainingSessionTrashedRecord->restore();
 
-            return back()->with('info', 'Data Restored Sucessfully');
-        }
+        return back()->with('info', 'Data Restored Sucessfully');
+    }
     
-        public function forceDelete($ctrlno)
-        {
-            $trainingSessionTrashedRecord = TrainingSession::onlyTrashed()->find($ctrlno);
-            $trainingSessionTrashedRecord->forceDelete();
+    public function forceDelete($ctrlno)
+    {
+        $trainingSessionTrashedRecord = TrainingSession::onlyTrashed()->find($ctrlno);
+        $trainingSessionTrashedRecord->forceDelete();
     
-            return back()->with('info', 'Data Permanently Deleted');
-        }
-    // end of  training session methods
+        return back()->with('info', 'Data Permanently Deleted');
+    }
 }
