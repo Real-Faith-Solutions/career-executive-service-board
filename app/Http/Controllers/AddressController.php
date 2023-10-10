c3b3e5b1b53eaaab5e6cab5c865e1ab6a4adbecd
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-10-10 13:46:11 +0800

    checking if addressProfile has personal data cesno

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -15,12 +15,20 @@ class AddressController extends Controller
     public function show($cesno){
 
         $addressProfile = ProfileAddress::where('personal_data_cesno', $cesno)->get();
-        $addressProfilePermanent = ProfileAddress::where('personal_data_cesno', $cesno)->where('type', 'Permanent')->first();
-        $addressProfileMailing = ProfileAddress::where('personal_data_cesno', $cesno)->where('type', 'Mailing')->first();
-        $addressProfileTemp = ProfileAddress::where('personal_data_cesno', $cesno)->where('type', 'Temporary')->first();
 
-        return view('admin.201_profiling.view_profile.partials.address.table', 
-        compact('addressProfile', 'addressProfilePermanent', 'addressProfileMailing', 'addressProfileTemp', 'cesno'));
+        if ($addressProfile->isEmpty()) {
+            // The $addressProfile variable is empty (no data found).
+            // You may want to handle this case accordingly.
+        } else {
+            $addressProfilePermanent = ProfileAddress::where('personal_data_cesno', $cesno)->where('type', 'Permanent')->first();
+            $addressProfileMailing = ProfileAddress::where('personal_data_cesno', $cesno)->where('type', 'Mailing')->first();
+            $addressProfileTemp = ProfileAddress::where('personal_data_cesno', $cesno)->where('type', 'Temporary')->first();
+
+            return view('admin.201_profiling.view_profile.partials.address.table', 
+            compact('addressProfile', 'addressProfilePermanent', 'addressProfileMailing', 'addressProfileTemp', 'cesno'));
+        }
+
+        
         
     }
 
