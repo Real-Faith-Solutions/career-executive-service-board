006db467f6a995a55db0c0d369c0b0efd31db81b
koykoy027 <villanuevajoshua27@gmail.com>
2023-10-12 14:31:34 +0800

    change colors

--- resources/views/admin/plantilla/reports/statistics/index.blade.php
+++ resources/views/admin/plantilla/reports/statistics/index.blade.php
@@ -17,8 +17,8 @@
             datasets: [
                 {
                     label: labels,
-                    backgroundColor: ["#1C64F2", "#0E8A16", "#D4C5F9", "#FBCA04"],
-                    borderColor: ["#1C64F2", "#0E8A16", "#D4C5F9", "#FBCA04"],
+                    backgroundColor: ["##1E90FF", "#8E44AD", "#2ECC71", "#FFA500"],
+                    borderColor: ["##1E90FF", "#8E44AD", "#2ECC71", "#FFA500"],
                     data: [{{ $totalMaleCESOChart }}, {{ $totalFemaleCESOChart }}, {{ $totalMaleNonCESOChart }}, {{ $totalFemaleNonCESOChart }}],
                     fill: false,
                 },
