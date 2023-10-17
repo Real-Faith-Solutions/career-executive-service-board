7555c53bc0b5fcb465f709361f8bf24fe4ac35c0
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-10-17 14:25:12 +0800

    FailedAttemp model modified

--- app/Models/FailedAttempt.php
+++ app/Models/FailedAttempt.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class FailedAttempt extends Model
 {
     use HasFactory;
+
+    protected $table = 'failed_attempts'; // Specify the table name
+
+    protected $fillable = [
+        'email',
+        'ip_address', 
+        'attempts', 
+        'suspension', 
+    ];
+    
 }
