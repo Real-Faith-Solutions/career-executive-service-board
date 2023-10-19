3574c302e7bbbc610c4b0b1dd19811a3b1c3508a
koykoy027 <villanuevajoshua27@gmail.com>
2023-10-19 15:50:17 +0800

    add margin bottom

--- resources/views/admin/plantilla/header.blade.php
+++ resources/views/admin/plantilla/header.blade.php
@@ -1,3 +1,3 @@
-<h1 class="text-center text-2xl font-semibold whitespace-nowrap uppercase text-blue-700">
+<h1 class="text-center text-2xl font-semibold whitespace-nowrap uppercase text-blue-700 mb-5">
   Plantilla Management System - (PMS)
 </h1>
