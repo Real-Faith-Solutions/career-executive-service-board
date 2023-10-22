419b2e219dc060ead59f17bac41c662e17c5a341
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-10-22 19:59:44 +0800

    displaying rank tracker in 201

--- resources/views/admin/201_profiling/view_profile/partials/eligibility_and_rank_tracker/validation_table.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/eligibility_and_rank_tracker/validation_table.blade.php
@@ -50,7 +50,7 @@
                     @foreach ($rapidValidation as $rapidValidations)
                         <tr class="border-b bg-white">
                             <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
-                                {{ \Carbon\Carbon::parse($rapidValidations->dteassign)->format('m/d/Y ') ?? 'No Record' }}-
+                                {{ \Carbon\Carbon::parse($rapidValidations->dteassign)->format('m/d/Y ') ?? 'No Record' }} -
                                 {{ \Carbon\Carbon::parse($rapidValidations->dtesubmit)->format('m/d/Y ') ?? 'No Record' }}
                             </td>
                                 
@@ -68,7 +68,7 @@
                     @foreach ($inDepthValidation as $inDepthValidations)
                     <tr class="border-b bg-white">
                         <td scope="row" class="whitespace-nowrap px-6 py-4 font-medium text-gray-900">
-                            {{ \Carbon\Carbon::parse($inDepthValidations->dteassign)->format('m/d/Y ') ?? 'No Record' }}
+                            {{ \Carbon\Carbon::parse($inDepthValidations->dteassign)->format('m/d/Y ') ?? 'No Record' }} -
                             {{ \Carbon\Carbon::parse($inDepthValidations->dtesubmit)->format('m/d/Y ') ?? 'No Record' }}
                         </td>
                         
