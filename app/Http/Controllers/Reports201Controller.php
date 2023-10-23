6c881bbf1f82e28f6d7d2ca2fc394e9d697faddd
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-10-23 12:42:37 +0800

    bugs fixed

--- app/Http/Controllers/Reports201Controller.php
+++ app/Http/Controllers/Reports201Controller.php
@@ -100,6 +100,7 @@ class Reports201Controller extends Controller
 
         return view('admin\201_profiling\reports\general_report', compact('personalData', 'query', 'sortBy', 'sortOrder',
                         'filter_active', 'filter_inactive', 'filter_retired', 'filter_deceased', 'filter_retirement',
-                        'with_pending_case', 'without_pending_case', 'profileLibTblCesStatus', 'cesstat_code', 'profileLibTblAppAuthority'));
+                        'with_pending_case', 'without_pending_case', 'profileLibTblCesStatus', 'cesstat_code', 
+                        'profileLibTblAppAuthority', 'authority_code'));
     }
 }
