fa7af9ed178bc23327f5254acf7f57de567ab492
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-03 16:12:28 +0800

    Pending Files icon

--- resources/views/admin/dashboard/partials/user_ceso_status.blade.php
+++ resources/views/admin/dashboard/partials/user_ceso_status.blade.php
@@ -132,32 +132,23 @@
         </div>
     </div>
 
-    <div class="card bg-green-100">
-        <div class="flex justify-between text-green-500 items-center">
+    <div class="card bg-teal-100">
+        <div class="flex justify-between text-teal-500 items-center">
             <div>
                 <p>Pending Files</p>
                 <h1 class="text-3xl font-bold">{{ $totalCESOActive }}</h1>
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                 stroke="currentColor" class="w-12 h-12 opacity-75">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                <path stroke-linecap="round" stroke-linejoin="round" 
+                    d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
             </svg>
-        </div>
-        <br>
-        <div>
-            <p class="text-sm">
-                {{-- Percentage of total CES employees. --}}
-            </p>
-            <span class="text-green-500 rounded opacity-75">
-                {{-- {{ $percentageCES }} % --}}
-            </span>
         </div>
     </div>
 
-    <div class="card bg-orange-100">
-        <div class="flex justify-between text-orange-500 items-center">
+    <div class="card bg-emerald-100">
+        <div class="flex justify-between text-emerald-500 items-center">
             <div>
                 <p>Accepted Files</p>
                 <h1 class="text-3xl font-bold">{{ $totalCESODeceased }}</h1>
@@ -169,14 +160,8 @@
                     d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
             </svg>
         </div>
-        <br>
-        <p class="text-sm">
-            {{-- Percentage of total non-CES employees --}}
-        </p>
-        <span class="text-orange-500 rounded opacity-75">
-            {{-- {{ $percentageNonCES }} % --}}
-        </span>
     </div>
+
     <div class="card bg-red-100">
         <div class="flex justify-between text-red-500 items-center">
             <div>
@@ -190,16 +175,10 @@
                     d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
             </svg>
         </div>
-        <br>
-        <p class="text-sm">
-            {{-- Percentage of total non-CES employees --}}
-        </p>
-        <span class="text-red-500 rounded opacity-75">
-            {{-- {{ $percentageNonCES }} % --}}
-        </span>
     </div>
-    <div class="card bg-violet-100">
-        <div class="flex justify-between text-violet-500 items-center">
+
+    <div class="card bg-purple-100">
+        <div class="flex justify-between text-purple-500 items-center">
             <div>
                 <p>Pending Case</p>
                 <h1 class="text-3xl font-bold">{{ $totalCESOInactive }}</h1>
@@ -211,12 +190,5 @@
                     d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
             </svg>
         </div>
-        <br>
-        <p class="text-sm">
-            {{-- Percentage of total non-CES employees --}}
-        </p>
-        <span class="text-violet-500 rounded opacity-75">
-            {{-- {{ $percentageNonCES }} % --}}
-        </span>
     </div>
 </div>
