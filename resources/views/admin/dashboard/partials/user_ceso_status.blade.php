3eef22cf2c0ff39ba4389282df52f2ad2670c020
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-03 15:42:25 +0800

    Research and Studies icons

--- resources/views/admin/dashboard/partials/user_ceso_status.blade.php
+++ resources/views/admin/dashboard/partials/user_ceso_status.blade.php
@@ -62,37 +62,21 @@
                     d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
             </svg>
         </div>
-        <br>
-        <div>
-            <p class="text-sm">
-                {{-- Percentage of total CES employees. --}}
-            </p>
-            <span class="text-green-500 rounded opacity-75">
-                {{-- {{ $percentageCES }} % --}}
-            </span>
-        </div>
     </div>
 
-    <div class="card bg-orange-100">
+    <div class="card bg-yellow-100">
         <div class="flex justify-between text-orange-500 items-center">
             <div>
                 <p>Research and Studies</p>
                 <h1 class="text-3xl font-bold">{{ $totalCESODeceased }}</h1>
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                 stroke="currentColor" class="w-12 h-12 opacity-75">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                <path stroke-linecap="round" stroke-linejoin="round" 
+                    d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
             </svg>
         </div>
-        <br>
-        <p class="text-sm">
-            {{-- Percentage of total non-CES employees --}}
-        </p>
-        <span class="text-orange-500 rounded opacity-75">
-            {{-- {{ $percentageNonCES }} % --}}
-        </span>
     </div>
     <div class="card bg-red-100">
         <div class="flex justify-between text-red-500 items-center">
