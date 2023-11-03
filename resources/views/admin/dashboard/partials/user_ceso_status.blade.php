00382cf615bbbb7243508b2c9a34ce416478ba30
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-03 16:19:54 +0800

    Pending Case icon

--- resources/views/admin/dashboard/partials/user_ceso_status.blade.php
+++ resources/views/admin/dashboard/partials/user_ceso_status.blade.php
@@ -101,12 +101,6 @@
                 <h1 class="text-3xl font-bold">{{ $totalCESOInactive }}</h1>
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-12 h-12 opacity-75">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
-            </svg>
-
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                 stroke="currentColor" class="w-12 h-12 opacity-75">
                 <path stroke-linecap="round" stroke-linejoin="round" 
@@ -169,25 +163,25 @@
                 <h1 class="text-3xl font-bold">{{ $totalCESORetired }}</h1>
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                 stroke="currentColor" class="w-12 h-12 opacity-75">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                <path stroke-linecap="round" stroke-linejoin="round" 
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
             </svg>
         </div>
     </div>
 
-    <div class="card bg-purple-100">
-        <div class="flex justify-between text-purple-500 items-center">
+    <div class="card bg-orange-100">
+        <div class="flex justify-between text-orange-500 items-center">
             <div>
                 <p>Pending Case</p>
                 <h1 class="text-3xl font-bold">{{ $totalCESOInactive }}</h1>
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                 stroke="currentColor" class="w-12 h-12 opacity-75">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                <path stroke-linecap="round" stroke-linejoin="round" 
+                    d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
             </svg>
         </div>
     </div>
