6090fde601e6eb85f15f7e1755fe72163d6f4349
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-11-05 11:29:52 +0800

    fixing generating report in assessment center

--- app/Http/Controllers/ERIS/Report/AssessmentCenterReportController.php
+++ app/Http/Controllers/ERIS/Report/AssessmentCenterReportController.php
@@ -26,10 +26,48 @@ class AssessmentCenterReportController extends Controller
 
     public function generateReportPdf(Request $request)
     {
-        $assessmentCenter = $this->dataFiltering($request);
+        $startDate = $request->input('startDate');
+        $endDate = $request->input('endDate');
+        $passed = $request->input('passed');
+        $failed = $request->input('failed');
+        $retake = $request->input('retake');
+
+        $assessmentCenter = AssessmentCenter::query();
+
+        $assessmentCenter->where(function ($query) use ($passed, $failed, $retake) {
+            if ($passed && $failed && $retake) {
+                $query->whereIn('remarks', [$passed, $failed])
+                    ->where('numtakes', '>', 1);
+            }
+            elseif($passed && $failed)
+            {
+                $query->whereIn('remarks', [$passed, $failed]);
+            }
+            else 
+            {
+                if ($passed) {
+                    $query->where('remarks', $passed);
+                }
+
+                if ($failed) {
+                    $query->where('remarks', $failed);
+                }
+
+                if ($retake) {
+                    $query->where('numtakes', '>', 1);
+                }
+            }
+        });
+
+        if ($startDate && $endDate) 
+        {
+            $assessmentCenter->whereBetween(DB::raw('CAST(acdate AS DATE)'), [$startDate, $endDate]);
+        }
+
+        $assessmentCenter = $assessmentCenter->orderBy('acdate')->get();
 
         $pdf = Pdf::loadView('admin.eris.reports.assessment_center.report_pdf', [
-            'assessmentCenter' => $assessmentCenter['assessmentCenter'],
+            'assessmentCenter' => $assessmentCenter,
         ])
         ->setPaper('a4', 'landscape');
 
