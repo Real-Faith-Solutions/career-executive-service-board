aeb7104e513dbf288ce4e6a8a6e7f4d5c6aa7265
koykoy027 <villanuevajoshua27@gmail.com>
2023-11-05 22:07:38 +0800

    test

--- app/Http/Controllers/Plantilla/Reports/StatisticsController.php
+++ app/Http/Controllers/Plantilla/Reports/StatisticsController.php
@@ -22,7 +22,7 @@ class StatisticsController extends Controller
         // planPosition.office.agencyLocation.departmentAgency
         $totalPosition = PlanAppointee::whereHas('planPosition', function ($query) use ($deptid) {
             $query->where('is_ces_pos', 1)
-                ->where('pres_apptee', 1)
+                // ->where('pres_apptee', 1)
                 ->whereHas('office.agencyLocation.departmentAgency', function ($query) use ($deptid) {
                     $query->where('mother_deptid', $deptid);
                 });
