a7194f91f0f1ed991127068c222d7808274761c7
koykoy027 <villanuevajoshua27@gmail.com>
2023-11-05 21:52:29 +0800

    test

--- app/Http/Controllers/Plantilla/Reports/StatisticsController.php
+++ app/Http/Controllers/Plantilla/Reports/StatisticsController.php
@@ -19,9 +19,16 @@ class StatisticsController extends Controller
 
         $motherDepartmentAgency = DepartmentAgency::find($deptid);
 
-        $totalPosition = PlanAppointee::whereHas('planPosition.office.agencyLocation.departmentAgency', function ($query) use ($deptid) {
-            $query->where('deptid', $deptid);
-        })->count();
+        // $totalPosition = PlanAppointee::whereHas('planPosition.office.agencyLocation.departmentAgency', function ($query) use ($deptid) {
+        //     $query->where('mother_deptid', $deptid);
+        // })->count();
+
+        $totalPosition = DepartmentAgency::whereHas('agencyLocation.office.planPosition.', function ($query) use ($deptid) {
+            $query->where('is_ces_pos', 1)
+                ->where('pres_apptee', 1);
+        })
+            ->where('deptid', $deptid)
+            ->count();
 
         $occupiedCESPosition = DepartmentAgency::whereHas('agencyLocation.office.planPosition.planAppointee', function ($query) {
             // unfinish
