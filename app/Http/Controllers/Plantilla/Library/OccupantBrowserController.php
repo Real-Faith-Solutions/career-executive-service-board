a956adac3ee1ef32655abea4e49e6dcd11a0c687
koykoy027 <villanuevajoshua27@gmail.com>
2023-11-09 11:14:00 +0800

    query result

--- app/Http/Controllers/Plantilla/Library/OccupantBrowserController.php
+++ app/Http/Controllers/Plantilla/Library/OccupantBrowserController.php
@@ -39,13 +39,13 @@ class OccupantBrowserController extends Controller
             $filterDropdown->whereHas('planPosition.positionMasterLibrary', function ($queryBuilder) use ($query, $officeDropdown) {
                 $queryBuilder->where(function ($subQuery) use ($query, $officeDropdown) {
                     if ($query) {
-                        $subQuery->where('dbm_title', 'LIKE', "%$query%");
+                        $subQuery->where('dbm_title', 'LIKE', "%$query%")
+                            ->orderBy('sg', 'desc');
                     }
 
                     if ($officeDropdown) {
-                        $subQuery->orWhereHas('planPosition.office', function ($officeQuery) use ($officeDropdown, $query) {
-                            $officeQuery->where('officeid', $officeDropdown)
-                                ->orWhere('dbm_title', 'LIKE', "%$query%");
+                        $subQuery->orWhereHas('planPosition', function ($officeQuery) use ($officeDropdown, $query) {
+                            $officeQuery->where('officeid', $officeDropdown);
                         });
                     }
                 });
