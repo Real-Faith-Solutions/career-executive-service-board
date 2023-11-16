626e5b7d24a005ff3e649baa4e332243310f5430
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-16 16:10:00 +0800

    color for graphs adjusted

--- resources/views/admin/dashboard/partials/ceso_status.blade.php
+++ resources/views/admin/dashboard/partials/ceso_status.blade.php
@@ -61,22 +61,8 @@
                             {{ $age56to65 }},
                             {{ $age66above }}
                         ],
-                    backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 205, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    ],
-                    borderColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 205, 86)',
-                    'rgb(75, 192, 192)',
-                    'rgb(54, 162, 235)',
-                    'rgb(153, 102, 255)',
-                    ],
+                    backgroundColor: ["#86efac", "#fdba74", "#fca5a5", "#c4b5fd", "#FFBC42", "#BBE1C3"],
+                    borderColor: ["#86efac", "#fdba74", "#fca5a5", "#c4b5fd", "#FFBC42", "#BBE1C3"],
                     fill: true,
                 },
 
