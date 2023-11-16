a762b60d337761641a7b4f32da5879327f250762
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-16 09:23:26 +0800

    package installed and login layout modified

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1 class="text-lg font-semibold text-center text-blue-500 mt-2">Welcome to CES Board</h1>
+<h1 class="text-xl font-semibold text-center text-blue-400 mt-2">Welcome to CES Board</h1>
 
 <form class="user" method="POST" action="{{ route('login') }}">
     @csrf
@@ -27,22 +27,28 @@
         @enderror
     </div>
 
-    <div class="flex justify-end">
-        <button class="btn btn-primary">Login</button>
+    <div class="mb-3 flex justify-start">
+        {!! htmlFormSnippet() !!}
+    </div>
+    
+    <div class="flex justify-center">
+        <button class="items-center py-2 px-20 uppercase tracking-widest text-sm hover:opacity-80 rounded bg-blue-500 text-white">Login</button>
     </div>
 
-    <div class="mt-4 flex items-center justify-end">
-        <input id="remember" type="checkbox" name="remember" value="true" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-        <label for="remember" class="ml-2 mt-2 text-sm font-medium text-gray-900">Remember Me</label>
-    </div>
+    <div class="flex justify-between">
+        <div class="flex items-center">
+            <input id="remember" type="checkbox" name="remember" value="true" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
+            <label for="remember" class="ml-2 mt-2 text-sm font-medium text-gray-900">Remember Me</label>
+        </div>
 
-    <div class="mt-4 flex items-center justify-end">
-        <a class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            href="{{ route('forgotPassword') }}">
-            Forgot your password?
-        </a>
+        <div class="flex items-center">
+            <a class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                href="{{ route('forgotPassword') }}">
+                Forgot your password?
+            </a>
+        </div>
     </div>
-
+        
 </form>
 
 @endsection
