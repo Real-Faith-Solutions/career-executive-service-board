08f6a0b336436aa453b94a081b1c84211010045e
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-22 20:07:10 +0800

    adjusted routes

--- routes/web.php
+++ routes/web.php
@@ -940,13 +940,18 @@ Route::middleware('auth', 'verify.email.and.device')->group(function () {
     });
     // End of ERIS Report routes
 
-    // Reports routes
+    // 201 Reports routes
     Route::prefix('reports')->group(function () {
 
         Route::prefix('executive-201-profile ')->group(function () {
 
             Route::get('general-reports', [Reports201Controller::class, 'index'])->name('general-reports.index');
-            Route::get('general-reports/pdf/{sortBy}/{sortOrder}/{filter_active}/{filter_inactive}/{filter_retired}/{filter_deceased}/{filter_retirement}/{with_pending_case}/{without_pending_case}/{cesstat_code}/{authority_code}', [Reports201Controller::class, 'generatePdf'])->name('general-reports.pdf');
+            Route::get('general-reports/pdf/
+                        {sortBy}/{sortOrder}/{filter_active}/{filter_inactive}/
+                        {filter_retired}/{filter_deceased}/{filter_retirement}/
+                        {with_pending_case}/{without_pending_case}/{cesstat_code}/
+                        {authority_code}', [Reports201Controller::class, 'generatePdf'])->name('general-reports.pdf');
+                        
         });
     });
     // End of Reports routes
