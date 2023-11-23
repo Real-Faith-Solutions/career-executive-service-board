eb1db50ce3cc849e8919d0f1ec705e9bab57708b
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-23 11:04:55 +0800

    Total Inactive CESOs card modified

--- resources/views/admin/dashboard/partials/ceso_status.blade.php
+++ resources/views/admin/dashboard/partials/ceso_status.blade.php
@@ -166,11 +166,12 @@
                 <h1 class="text-3xl font-bold">{{ $totalCESOInactive }}</h1>
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                 stroke="currentColor" class="w-12 h-12 opacity-75">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                <path stroke-linecap="round" stroke-linejoin="round" 
+                    d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
             </svg>
+              
         </div>
     </div>
 
