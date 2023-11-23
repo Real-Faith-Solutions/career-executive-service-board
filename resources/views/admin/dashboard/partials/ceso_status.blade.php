29d503411af08a5b531c8ba8debaa2ca2d226dfc
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-23 10:47:13 +0800

    Total Active CESOs card modified

--- resources/views/admin/dashboard/partials/ceso_status.blade.php
+++ resources/views/admin/dashboard/partials/ceso_status.blade.php
@@ -119,10 +119,10 @@
                 <h1 class="text-3xl font-bold">{{ $totalCESOActive }}</h1>
             </div>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" 
                 stroke="currentColor" class="w-12 h-12 opacity-75">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                <path stroke-linecap="round" stroke-linejoin="round" 
+                    d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
             </svg>
         </div>
     </div>
