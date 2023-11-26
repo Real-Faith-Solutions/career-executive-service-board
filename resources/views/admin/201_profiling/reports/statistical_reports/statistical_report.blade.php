57e4c687b9d77161cda1433ea93a52efe154dc99
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-26 23:58:41 +0800

    STATISTICAL REPORTS modified

--- resources/views/admin/201_profiling/reports/statistical_reports/statistical_report.blade.php
+++ resources/views/admin/201_profiling/reports/statistical_reports/statistical_report.blade.php
@@ -6,15 +6,11 @@
 <nav class="bg-gray-200 border-gray-200 mb-3">
     <div class="flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" class="flex items-center">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap uppercase text-blue-500">201 PROFILE - GENERAL REPORTS</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap uppercase text-blue-500">201 PROFILE - STATISTICAL REPORTS</span>
         </a>
 
         <div class="flex justify-end">
-            <a href="{{ route('general-reports.pdf', 
-                ['sortBy' => $sortBy, 'sortOrder' => $sortOrder, 'filter_active' => $filter_active, 'filter_inactive' => $filter_inactive, 
-                 'filter_retired' => $filter_retired, 'filter_deceased' => $filter_deceased, 'filter_retirement' => $filter_retirement, 
-                 'with_pending_case' => $with_pending_case, 'without_pending_case' => $without_pending_case, 
-                 'cesstat_code' => $cesstat_code, 'authority_code' => $authority_code]) }}" target='_blank' class="btn btn-primary">Generate PDF Report</a>
+            <a href="#" target='_blank' class="btn btn-primary">Generate PDF Report</a>
         </div>
     </div>
 </nav>
@@ -113,182 +109,19 @@
                 <thead class="bg-gray-50 text-xs uppercase text-gray-700">
                     <tr>
                         <th scope="col" class="px-6 py-3">
-                            <a href="{{ route('general-reports.index', [
-                                            'sort_by' => 'cesno',
-                                            'sort_order' => $sortOrder === 'asc' ? 'desc' : 'asc',
-                                            'filter_active' => $filter_active,
-                                            'filter_inactive' => $filter_inactive,
-                                            'filter_retired' => $filter_retired,
-                                            'filter_deceased' => $filter_deceased,
-                                            'filter_retirement' => $filter_retirement,
-                                            'with_pending_case' => $with_pending_case,
-                                            'without_pending_case' => $without_pending_case,
-                                            'cesstat_code' => $cesstat_code,
-                                            'authority_code' => $authority_code,
-                                        ]) }}" class="flex items-center space-x-1">
-                                Ces No. {{ $filter_active }}
-                                @if ($sortBy === 'cesno')
-                                    @if ($sortOrder === 'asc')
-                                        <svg class="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
-                                        </svg>
-                                    @else
-                                        <svg class="w-4 h-4 text-gray-500 transform rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
-                                        </svg>
-                                    @endif
-                                @endif
-                            </a>
+                            sample1
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            <a href="{{ route('general-reports.index', [
-                                            'sort_by' => 'lastname', 
-                                            'sort_order' => $sortOrder === 'asc' ? 'desc' : 'asc',
-                                            'filter_active' => $filter_active,
-                                            'filter_inactive' => $filter_inactive,
-                                            'filter_retired' => $filter_retired,
-                                            'filter_deceased' => $filter_deceased,
-                                            'filter_retirement' => $filter_retirement,
-                                            'with_pending_case' => $with_pending_case,
-                                            'without_pending_case' => $without_pending_case,
-                                            'cesstat_code' => $cesstat_code,
-                                            'authority_code' => $authority_code,
-                                        ]) }}" class="flex items-center space-x-1">
-                                Name
-                                @if ($sortBy === 'lastname')
-                                    @if ($sortOrder === 'asc')
-                                        <svg class="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
-                                        </svg>
-                                    @else
-                                        <svg class="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                                        </svg>
-                                    @endif
-                                @endif
-                            </a>
+                            sample2
                         </th>
 
-                        @if ($filter_active == "true" || $filter_inactive == "true" || $filter_retired == "true" || $filter_deceased == "true")
-                            <th scope="col" class="px-6 py-3">
-                                <span class="">Status</span>
-                            </th>
-                        @endif
-
-                        @if ($cesstat_code !== "false")
-                            <th scope="col" class="px-6 py-3">
-                                <span class="">CES Status</span>
-                            </th>
-                        @endif
-
-                        @if ($authority_code !== "false")
-                            <th scope="col" class="px-6 py-3">
-                                <span class="">Appointing Authority</span>
-                            </th>
-                        @endif
-
-                        @if ($with_pending_case == "true")
-                            <th scope="col" class="px-6 py-3">
-                                <span class="">Pending Case</span>
-                            </th>
-                        @endif
                         
                     </tr>
                 </thead>
                 <tbody>
-                        @foreach ($personalData as $personalDatas)
-                            <tr class="border-b bg-white hover:bg-slate-400 hover:text-white">
-                                <th scope="col" class="px-6 py-3">
-                                    {{ $personalDatas->cesno }}
-                                </th>
-
-                                <td scope="col" class="px-6 py-3">
-                                    {{ $personalDatas->lastname }}, {{ $personalDatas->firstname }} {{ $personalDatas->middlename }}
-                                </td>
-
-                                @if ($filter_active == "true" || $filter_inactive == "true" || $filter_retired == "true" || $filter_deceased == "true")
-                                    <td scope="col" class="px-6 py-3">
-                                        {{ $personalDatas->status ?? '' }}
-                                    </td>
-                                @endif  
-
-                                @if ($cesstat_code !== "false")
-                                    <td scope="col" class="px-6 py-3">
-                                        {{ $personalDatas->cesStatus->description ?? 'none' }}
-                                    </td>
-                                @endif
-
-                                @if ($authority_code !== "false")
-                                    <td scope="col" class="px-6 py-3">
-                                        {{ $personalDatas->getAppointingAuthorityDescription($personalDatas) ?? 'none' }}
-                                    </td>
-                                @endif
-
-                                @if ($with_pending_case == "true")
-                                    <td scope="col" class="px-6 py-3">
-                                        @if ($personalDatas->caseRecords->isNotEmpty())
-
-                                            @php
-                                                $pendingCount = 0; 
-                                            @endphp
-
-                                            @foreach ($personalDatas->caseRecords as $caseRecord)
-                                                @if ($caseRecord->caseStatusCode->TITLE === 'Pending')
-                                                    @php
-                                                        $pendingCount++;
-                                                    @endphp
-                                                @endif
-                                            @endforeach
-
-                                            @if ($pendingCount > 0)
-                                                {{ $pendingCount }} pending case
-                                            @else
-                                                none
-                                            @endif
-
-                                            {{-- @foreach ($personalDatas->caseRecords as $caseRecord)
-
-                                                @if ($caseRecord->caseStatusCode->TITLE !== 'Pending' && $loop->remaining <= 0 && !$loop->first)
-                                                    
-                                                @elseif ($caseRecord->caseStatusCode->TITLE !== 'Pending' && $loop->remaining <= 0)
-                                                    none
-                                                @elseif ($caseRecord->caseStatusCode->TITLE !== 'Pending' && $loop->remaining > 0)
-                                                    
-                                                @elseif ($caseRecord->caseStatusCode->TITLE == 'Pending' && $loop->first)
-                                                    {{ $caseRecord->offence }},
-                                                @elseif ($caseRecord->caseStatusCode->TITLE == 'Pending' && !$loop->first && !$loop->last)
-                                                     {{ $caseRecord->offence }},
-                                                @elseif ($caseRecord->caseStatusCode->TITLE == 'Pending' && $loop->last)
-                                                     {{ $caseRecord->offence }}
-                                                @endif
-
-                                            @endforeach --}}
-
-                                        @else
-                                            none
-                                        @endif
-                                    </td>
-                                @endif
-
-                            </tr>
-                        @endforeach
+                        
                 </tbody>
             </table>
-            <div class="my-5">
-                {{ $personalData->appends([
-                    'sort_by' => $sortBy, 
-                    'sort_order' => $sortOrder,
-                    'filter_active' => $filter_active,
-                    'filter_inactive' => $filter_inactive,
-                    'filter_retired' => $filter_retired,
-                    'filter_deceased' => $filter_deceased,
-                    'filter_retirement' => $filter_retirement,
-                    'with_pending_case' => $with_pending_case,
-                    'without_pending_case' => $without_pending_case,
-                    'cesstat_code' => $cesstat_code,
-                    'authority_code' => $authority_code,
-                    ])->links() }}
-            </div>
         </div>
 
     </section>
