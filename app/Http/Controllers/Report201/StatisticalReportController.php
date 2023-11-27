473624293cad646147413867a307f5986e9bab60
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-11-27 14:01:12 +0800

    StatisticalReportController added index

--- app/Http/Controllers/Report201/StatisticalReportController.php
+++ app/Http/Controllers/Report201/StatisticalReportController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class StatisticalReportController extends Controller
 {
-    //
+    
+    public function index(Request $request)
+    {
+
+        return view('admin.201_profiling.reports.statistical_reports.statistical_report');
+
+    }
+
 }
