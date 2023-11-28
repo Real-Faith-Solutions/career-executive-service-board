ffa51b2db563aa4428ed5d9da24e865478ce3e85
koykoy027 <villanuevajoshua27@gmail.com>
2023-11-28 17:55:31 +0800

    other assignment requery

--- resources/views/admin/plantilla/appointee_occupant_browser/edit.blade.php
+++ resources/views/admin/plantilla/appointee_occupant_browser/edit.blade.php
@@ -83,7 +83,7 @@
     </ol>
 </nav>
 
-<div class="flex justify-between">
+<div class="flex justify-between mb-3">
     <a href="#" class="text-blue-500 uppercase text-2xl">
         Other Assignment
     </a>
@@ -93,90 +93,95 @@
     @include('admin.plantilla.other_assignment.create')
 </div>
 
-@include('layouts.partials.isLoading')
-<table class="dataTables hidden">
-    <thead>
-        <tr>
-            <th>Detailed ID</th>
-            <th>Status</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Remarks</th>
-            <th>From - To</th>
-            <th>Flr / Bldg</th>
-            <th>Street</th>
-            <th>Barangay</th>
-            <th>City</th>
-            <th>Contact number</th>
-            <th>Email</th>
-            <th>CESNO</th>
+{{-- @include('layouts.partials.isLoading') --}}
+<div class="table-responsive">
+    <table class="dataTables">
+        <thead>
+            <tr>
+                <th>Detailed ID</th>
+                <th>Status</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Remarks</th>
+                <th>From - To</th>
+                <th>Flr / Bldg</th>
+                <th>Street</th>
+                <th>Barangay</th>
+                <th>City</th>
+                <th>Contact number</th>
+                <th>Email</th>
+                <th>CESNO</th>
 
-            <th>
-                <span class="sr-only">Action</span>
-            </th>
-        </tr>
-    </thead>
-    <tbody>
+                <th>
+                    <span class="sr-only">Action</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
 
-        @foreach ($otherAssignment as $data)
+            @foreach ($otherAssignment as $data)
 
 
-        <tr>
-            <td class="font-semibold">
-                {{ $data->detailed_code }}
-            </td>
+            <tr>
+                <td class="font-semibold">
+                    {{ $data->detailed_code }}
+                </td>
 
-            <td>{{ $data->apptStatus->title ?? ''}}</td>
-            <td>{{ $data->position ?? ''}}</td>
-            <td>{{ $data->office ?? ''}}</td>
-            <td>{{ $data->remarks ?? ''}}</td>
-            <td>
-                {{ \Carbon\Carbon::parse($data->from_dt)->format('m/d/Y') }} -
-                {{ \Carbon\Carbon::parse($data->to_dt)->format('m/d/Y') }}
-            </td>
-            <td>{{ $data->house_bldg ?? ''}}</td>
-            <td>{{ $data->st_road ?? ''}}</td>
-            <td>{{ $data->brgy_vill ?? ''}}</td>
-            <td>{{ $data->cities->name ?? ''}}</td>
-            <td>{{ $data->contactno ?? ''}}</td>
-            <td>{{ $data->email_addr ?? ''}}</td>
-            <td>{{ $data->cesno ?? ''}}</td>
+                <td>{{ $data->apptStatus->title ?? ''}}</td>
+                <td>{{ $data->position ?? ''}}</td>
+                <td>{{ $data->office ?? ''}}</td>
+                <td>{{ $data->remarks ?? ''}}</td>
+                <td>
+                    {{ \Carbon\Carbon::parse($data->from_dt)->format('m/d/Y') }} -
+                    {{ \Carbon\Carbon::parse($data->to_dt)->format('m/d/Y') }}
+                </td>
+                <td>{{ $data->house_bldg ?? ''}}</td>
+                <td>{{ $data->st_road ?? ''}}</td>
+                <td>{{ $data->brgy_vill ?? ''}}</td>
+                <td>{{ $data->cities->name ?? ''}}</td>
+                <td>{{ $data->contactno ?? ''}}</td>
+                <td>{{ $data->email_addr ?? ''}}</td>
+                <td>{{ $data->cesno ?? ''}}</td>
 
-            <td class="text-right uppercase">
-                <div class="flex justify-end">
-                    <a class="hover:bg-slate-100 rounded-full" href="{{ route('other-assignment.show', [
-                        'sectorid' => $sector->sectorid,
-                        'deptid' => $department->deptid,
-                        'officelocid' => $departmentLocation->officelocid,
-                        'officeid' => $office->officeid,
-                        'plantilla_id' => $planPosition->plantilla_id,
-                        'appointee_id' => $appointees->appointee_id,
-                        'detailed_code' => $data->detailed_code,
-                    ]) }}" title="View assignment">
-                        <lord-icon src="https://cdn.lordicon.com/bxxnzvfm.json" trigger="hover"
-                            colors="primary:#3a3347,secondary:#ffc738,tertiary:#f9c9c0,quaternary:#ebe6ef"
-                            style="width:24px;height:24px">
-                        </lord-icon>
-                    </a>
-                    <form class="hover:bg-slate-100 rounded-full"
-                        action="{{ route('other-assignment.destroy', ['detailed_code' => $data->detailed_code]) }}"
-                        method="POST" onsubmit="return window.confirm('Are you sure you want to delete this item?')">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit" class="mx-1 font-medium text-red-600 hover:underline"
-                            title="Delete Assignment">
-                            <lord-icon src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover"
-                                colors="primary:#DC3545" style="width:24px;height:24px">
+                <td class="text-right uppercase">
+                    <div class="flex justify-end">
+                        <a class="hover:bg-slate-100 rounded-full" href="{{ route('other-assignment.show', [
+                            'sectorid' => $sector->sectorid,
+                            'deptid' => $department->deptid,
+                            'officelocid' => $departmentLocation->officelocid,
+                            'officeid' => $office->officeid,
+                            'plantilla_id' => $planPosition->plantilla_id,
+                            'appointee_id' => $appointees->appointee_id,
+                            'detailed_code' => $data->detailed_code,
+                        ]) }}" title="View assignment">
+                            <lord-icon src="https://cdn.lordicon.com/bxxnzvfm.json" trigger="hover"
+                                colors="primary:#3a3347,secondary:#ffc738,tertiary:#f9c9c0,quaternary:#ebe6ef"
+                                style="width:24px;height:24px">
                             </lord-icon>
-                        </button>
-                    </form>
-                </div>
-            </td>
-        </tr>
-        @endforeach
-        {{-- @endforeach --}}
+                        </a>
+                        <form class="hover:bg-slate-100 rounded-full"
+                            action="{{ route('other-assignment.destroy', ['detailed_code' => $data->detailed_code]) }}"
+                            method="POST" onsubmit="return window.confirm('Are you sure you want to delete this item?')">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="mx-1 font-medium text-red-600 hover:underline"
+                                title="Delete Assignment">
+                                <lord-icon src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover"
+                                    colors="primary:#DC3545" style="width:24px;height:24px">
+                                </lord-icon>
+                            </button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+            {{-- @endforeach --}}
 
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
 
+<div class="m-5">
+    {{ $otherAssignment->links() }}
+</div>
 @endsection
