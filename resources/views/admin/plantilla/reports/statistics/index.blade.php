c9f17a22f0d77f829b8d2de05bf131e518ab08b5
koykoy027 <villanuevajoshua27@gmail.com>
2023-11-29 09:34:50 +0800

    add svg fr bluebook statistics

--- resources/views/admin/plantilla/reports/statistics/index.blade.php
+++ resources/views/admin/plantilla/reports/statistics/index.blade.php
@@ -231,31 +231,55 @@
                                         </a>
                                     </td>
                                     <td class="px-6 py-4 whitespace-no-wrap text-sm">
-                                        <div class="flex "
-                                        <a class="text-gray-900 hover:text-blue-500" href="{{ route('statistics.pdf', $data->deptid) }}" target="_blank" title="Generate Department Bluebook report">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
-                                            </svg>
-                                        </a>
+                                        <div class="flex gap-2">
+                                            <a class="text-gray-900 hover:text-blue-500"
+                                                href="{{ route('statistics.pdf', $data->deptid) }}" target="_blank"
+                                                title="Generate Department Bluebook report">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
+                                                </svg>
+                                            </a>
+                                        </div>
                                     </td>
                                 </tr>
                                 @endforeach
 
                                 <tr>
                                     <td class="px-6 py-4 whitespace-no-wrap">
-                                        <a class="text-sm leading-5 text-gray-900 hover:text-blue-500"
-                                            href="{{ route('ces-occupancy-statistics-report.pdf', 'detailed occupancy') }}"
-                                            target="_blank" title="Generate PDF">
+                                        <a class="text-sm leading-5 text-gray-900 hover:text-blue-500" href="#">
                                             CES Occupancy Statistics Report
                                         </a>
                                     </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap">
+                                        <a class="text-sm leading-5 text-gray-900 hover:text-blue-500"
+                                            href="{{ route('ces-occupancy-statistics-report.pdf', 'detailed occupancy') }}"
+                                            target="_blank" title="Generate Statistics Summary">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
+                                            </svg>
+                                        </a>
+                                    </td>
                                 </tr>
+
                                 <tr>
+                                    <td class="px-6 py-4 whitespace-no-wrap">
+                                        <a class="text-sm leading-5 text-gray-900 hover:text-blue-500" href="#">
+                                            CES Occupancy Statistics Report Summary
+                                        </a>
+                                    </td>
                                     <td class="px-6 py-4 whitespace-no-wrap">
                                         <a class="text-sm leading-5 text-gray-900 hover:text-blue-500"
                                             href="{{ route('ces-occupancy-statistics-report-summary.pdf', 'occupancy') }}"
-                                            target="_blank" title="Generate PDF">
-                                            CES Occupancy Statistics Report Summary
+                                            target="_blank" title="Generate Detailed Occupancy Statistics">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
+                                            </svg>
                                         </a>
                                     </td>
                                 </tr>
