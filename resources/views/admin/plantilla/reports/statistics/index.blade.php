8663d0eb6ed1becf4d5ed3d6e522a4b884653140
koykoy027 <villanuevajoshua27@gmail.com>
2023-11-29 09:22:39 +0800

    rush rush rush

--- resources/views/admin/plantilla/reports/statistics/index.blade.php
+++ resources/views/admin/plantilla/reports/statistics/index.blade.php
@@ -216,16 +216,26 @@
                                         class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                         NATIONAL GOVERNMENT AGENCIES
                                     </th>
+                                    <th
+                                        class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                        Generate reports
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
                                 @foreach ($motherDepartmentAgency as $data)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-no-wrap">
-                                        <a class="text-sm leading-5 text-gray-900 hover:text-blue-500"
-                                            href="{{ route('statistics.pdf', $data->deptid) }}" target="_blank"
-                                            title="Generate PDF">
+                                        <a class="text-sm leading-5 text-gray-900 hover:text-blue-500" href="#">
                                             {{ $data->title }}
+                                        </a>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap text-sm">
+                                        <div class="flex "
+                                        <a class="text-gray-900 hover:text-blue-500" href="{{ route('statistics.pdf', $data->deptid) }}" target="_blank" title="Generate Department Bluebook report">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
+                                            </svg>
                                         </a>
                                     </td>
                                 </tr>
