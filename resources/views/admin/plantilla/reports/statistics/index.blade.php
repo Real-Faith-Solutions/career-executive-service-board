909076511649dffdd117f5d7403e9cb67bdb2c6e
koykoy027 <villanuevajoshua27@gmail.com>
2023-11-29 10:02:33 +0800

    generate reports in single page

--- resources/views/admin/plantilla/reports/statistics/index.blade.php
+++ resources/views/admin/plantilla/reports/statistics/index.blade.php
@@ -235,14 +235,46 @@
                                             <a class="text-gray-900 hover:text-blue-500"
                                                 href="{{ route('statistics.pdf', $data->deptid) }}" target="_blank"
                                                 title="Generate Department Bluebook report">
+                                                <!-- Add a unique ID for this link -->
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                     <path stroke-linecap="round" stroke-linejoin="round"
                                                         d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
                                                 </svg>
                                             </a>
+
+                                            <a class="text-gray-900 hover:text-blue-500"
+                                                href="{{ route('occupancy-report.pdf', $data->deptid) }}"
+                                                target="_blank" title="Generate Agency Bluebook report">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+                                                </svg>
+                                            </a>
+
+                                            <!-- Use a unique ID for this link -->
+                                            <a class="text-gray-900 hover:text-blue-500"
+                                                id="multiLink_{{ $loop->index }}" href="#"
+                                                title="Extract Department Bluebook Report Forms">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                                                </svg>
+                                            </a>
                                         </div>
                                     </td>
+
+                                    <script>
+                                        // Use the unique ID for this link in JavaScript
+                                        document.getElementById("multiLink_{{ $loop->index }}").addEventListener("click", function() {
+                                            window.open("{{ route('ceso-eligibles-ces-position.pdf', $data->deptid) }}", "_blank");
+                                            window.open("{{ route('ceso-eligibles-nonces-position.pdf', $data->deptid) }}", "_blank");
+                                            window.open("{{ route('nonceso-noneligibles-ces-position.pdf', $data->deptid) }}", "_blank");
+                                            window.open("{{ route('vacant-position.pdf', $data->deptid) }}", "_blank");
+                                        });
+                                    </script>
                                 </tr>
                                 @endforeach
 
@@ -255,7 +287,7 @@
                                     <td class="px-6 py-4 whitespace-no-wrap">
                                         <a class="text-sm leading-5 text-gray-900 hover:text-blue-500"
                                             href="{{ route('ces-occupancy-statistics-report.pdf', 'detailed occupancy') }}"
-                                            target="_blank" title="Generate Statistics Summary">
+                                            target="_blank" title="Generate Detailed Occupancy Statistics">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -274,7 +306,7 @@
                                     <td class="px-6 py-4 whitespace-no-wrap">
                                         <a class="text-sm leading-5 text-gray-900 hover:text-blue-500"
                                             href="{{ route('ces-occupancy-statistics-report-summary.pdf', 'occupancy') }}"
-                                            target="_blank" title="Generate Detailed Occupancy Statistics">
+                                            target="_blank" title="Generate Statistics Summary">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
