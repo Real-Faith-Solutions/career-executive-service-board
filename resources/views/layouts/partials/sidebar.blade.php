295a9d0ace7fd8b3bd9164001ef784a36be446d1
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 17:14:24 +0800

    competency-report/resource-speaker-manager

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -495,7 +495,7 @@
 
                             <li>
                                 <a href="{{ route('competency-management-sub-modules-report.resourceSpeakerIndexReport') }}"
-                                    class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                    class="{{ request()->is('competency-report/resource-speaker-manager*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                     Resource Speaker Manager Reports
                                 </a>
                             </li>
