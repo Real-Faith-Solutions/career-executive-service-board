7405980286a6165b5a58dfa753392d974de0e039
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 14:12:05 +0800

    201-library/indigeneous-group

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -138,7 +138,7 @@
 
                         <li>
                             <a href="{{ route('indigeneous-group.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/indigeneous-group*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Indigenous Group
                             </a>
                         </li>
