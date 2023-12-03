56cecba62ba84a2e68227b6ecfaaf8ea5c927852
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 13:42:33 +0800

    201-library/record-status*

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -124,7 +124,7 @@
 
                         <li>
                             <a href="{{ route('record-status.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/record-status*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Record Status
                             </a>
                         </li>
