f7805b0b6875a549b7de5c4ac62c67bfb9003a95
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 16:58:57 +0800

    competency-report/general-report

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -474,7 +474,7 @@
                         <li>
                             <li>
                                 <a href="{{ route('competency-management-sub-modules-report.generalReportIndex') }}"
-                                    class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                    class="{{ request()->is('competency-report/general-report*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                     General Report
                                 </a>
                             </li>
