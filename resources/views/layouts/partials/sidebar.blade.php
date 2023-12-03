eddcf99a05131d3966733f2d183d5deaf0567b88
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 14:47:17 +0800

    201-library/expertise-general-library

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -242,7 +242,7 @@
 
                         <li>
                             <a href="{{ route('expertise-general.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/expertise-general-library*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Expertise General
                             </a>
                         </li>
