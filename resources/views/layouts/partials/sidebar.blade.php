54827f4dcf7acd35c1d71987a6935b28fe0f7e0b
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 17:12:47 +0800

    competency-report/training-venue-manager*

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -481,7 +481,7 @@
 
                             <li>
                                 <a href="{{ route('competency-management-sub-modules-report.trainingVenueManagerReportIndex') }}"
-                                    class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                    class="{{ request()->is('competency-report/training-venue-manager*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                     Training Venue Manager Reports
                                 </a>
                             </li>
