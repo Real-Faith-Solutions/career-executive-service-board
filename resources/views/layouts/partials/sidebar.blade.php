44a00df7aec0b580d33df918e815bd223c8659b0
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:16:11 +0800

    plantilla/library/library-position-manager

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -561,7 +561,7 @@
                             </a>
 
                             <a href="{{ route('library-position-manager.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/library/library-position-manager*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Plantilla Position Manager
                             </a>
 
