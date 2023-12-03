8817424952cb887b2deef7e3a9424605ab596416
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:28:04 +0800

    plantilla/library/library-mother-dept

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -576,7 +576,7 @@
                             </a>
 
                             <a href="{{ route('library-mother-dept.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/library/library-mother-dept*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Mother Agency
                             </a>
 
