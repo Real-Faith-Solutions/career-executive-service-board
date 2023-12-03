4384b34802e6442b548693293fcb9b42069224db
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 14:18:57 +0800

    201-library/educational-schools

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -165,7 +165,7 @@
 
                         <li>
                             <a href="{{ route('educational-schools.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/educational-schools*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Educational Schools
                             </a>
                         </li>
