54442e73757718c7f14acdc04f9ea0684d71e1d6
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 14:45:21 +0800

    201-library/case-status-library

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -235,7 +235,7 @@
 
                         <li>
                             <a href="{{ route('case-status-library.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/case-status-library*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Case Status
                             </a>
                         </li>
