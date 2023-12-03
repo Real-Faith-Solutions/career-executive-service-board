dec8595c189debdaa29785474f789e99a2ed0670
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 13:34:25 +0800

    201-library/civil-status

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -110,7 +110,7 @@
 
                         <li>
                             <a href="{{ route('civil-status.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/civil-status*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Civil Status
                             </a>
                         </li>
