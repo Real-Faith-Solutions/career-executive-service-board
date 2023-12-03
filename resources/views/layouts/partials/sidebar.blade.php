1a1f187b877e5be584dbe4ca32ec9ad1fbafcd3d
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:34:04 +0800

    plantilla/library/library-personnel-movement

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -601,7 +601,7 @@
                             </a>
 
                             <a href="{{ route('library-personnel-movement.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/library/library-personnel-movement*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Personnel Movement
                             </a>
                         </li>
