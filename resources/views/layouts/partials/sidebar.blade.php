1a4584a997ca5ccb8d7dbe73fcb8cc6f9248afe0
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:14:16 +0800

    plantilla/library/library-office-manager

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -556,7 +556,7 @@
                             </a>
 
                             <a href="{{ route('library-office-manager.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/library/library-office-manager*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Office Manager
                             </a>
 
