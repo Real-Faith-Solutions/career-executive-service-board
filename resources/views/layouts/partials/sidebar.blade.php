7457a15c8166711a27df3e09a7c889c87eadb136
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 15:29:01 +0800

    201-reports/statistical-reports*

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -290,7 +290,7 @@
 
                         <li>
                             <a href="{{ route('statistical-report.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-reports/statistical-reports*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Statistical Reports
                             </a>
                         </li>
