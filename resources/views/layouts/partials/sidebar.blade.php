f4e8736fbefe0fe86f3b28fe8fd4402db18fd35c
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:31:13 +0800

    plantilla/library/library-dbm-position-title

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -591,7 +591,7 @@
                             </a>
 
                             <a href="{{ route('library-dbm-position-title.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/library/library-dbm-position-title*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 DBM Position Title
                             </a>
 
