deae06f58609b6790f84decea8af3030a2fd731c
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 19:08:47 +0800

    plantilla/reports/vacant-position

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -658,7 +658,7 @@
                             </a>
 
                             <a href="{{ route('vacant-position.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/reports/vacant-position*') ? 'bg-gray-100' : '' }} group group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 List of Vacant Position
                             </a>
                         </li>
