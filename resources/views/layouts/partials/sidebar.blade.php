e40484a693b9126dd726d447451e6c00fdd0fb21
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 13:37:23 +0800

    201-library/title

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -117,7 +117,7 @@
 
                         <li>
                             <a href="{{ route('title.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/title*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Title
                             </a>
                         </li>
