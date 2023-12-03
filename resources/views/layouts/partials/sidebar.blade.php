ec68a9009e1e61cd0b045fc653e08b643f96ae87
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:55:02 +0800

    plantilla/reports/ceso-eligibles-ces-position

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -643,7 +643,7 @@
                             </a>
 
                             <a href="{{ route('ceso-eligibles-ces-position.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/reports/ceso-eligibles-ces-position*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 List of CESOs and CES Eligibles in CES Positions
                             </a>
 
