626800c4441839e0bb99406701e66358e3e9889b
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:25:36 +0800

    plantilla/library/library-occupant-manager

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -571,7 +571,7 @@
                             </a>
 
                             <a href="{{ route('library-occupant-manager.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/library/library-occupant-manager*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Appointee - Occupant Manager
                             </a>
 
