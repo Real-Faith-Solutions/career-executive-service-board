8cecc2f7d0238cba39955028d780ec06d7552db3
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 18:08:50 +0800

    plantilla/library/library-department-manager

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -546,7 +546,7 @@
                             </a>
 
                             <a href="{{ route('library-department-manager.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('plantilla/library/library-department-manager*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Department / Agency Manager
                             </a>
 
