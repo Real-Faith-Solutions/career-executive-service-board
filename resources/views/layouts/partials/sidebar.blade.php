bf19c05179d5ae2af18ff20819670c6baefe9380
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-03 14:47:44 +0800

    201-library/expertise-specialization-library

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -249,7 +249,7 @@
 
                         <li>
                             <a href="{{ route('expertise-specialization.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-library/expertise-specialization-library*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Expertise Specialization
                             </a>
                         </li>
