d29bde6b6aeddb0b686bcdd57ae4f8de4e6492a3
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-04 13:49:33 +0800

    201-reports/birthday

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -304,7 +304,7 @@
 
                         <li>
                             <a href="{{ route('birthday.index') }}"
-                                class="group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
+                                class="{{ request()->is('201-reports/birthday*') ? 'bg-gray-100' : '' }} group flex w-full items-center rounded-lg p-2 pl-11 text-gray-900 transition duration-75 hover:bg-gray-100">
                                 Reports for Birthday Cards
                             </a>
                         </li>
