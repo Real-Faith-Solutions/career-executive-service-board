f299d442ed1ffc108dcb6a6edbcfcec074e0cee2
manueljonrolando@gmail.com <manueljonrolando@gmail.com>
2023-12-09 22:45:58 +0800

    adding page count in written exam  reports

--- app/Http/Controllers/ERIS/Report/WrittenExamReportController.php
+++ app/Http/Controllers/ERIS/Report/WrittenExamReportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Eris\WrittenExam;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 
 class WrittenExamReportController extends Controller
@@ -44,8 +45,8 @@ class WrittenExamReportController extends Controller
         $failed = $request->input('failed');
         $retake = $request->input('retake');
         $location = $request->input('location');
-        $sortBy = $request->input('sortBy', 'we_date'); // Default sorting we_date.
-        $sortOrder = $request->input('sortOrder', 'desc'); // Default sorting order
+        $sortBy = $request->input('sortBy', 'lastname'); // Default sorting we_date.
+        $sortOrder = $request->input('sortOrder', 'asc'); // Default sorting order
 
         $writtenExam = WrittenExam::query();
 
@@ -118,85 +119,11 @@ class WrittenExamReportController extends Controller
         ];
     }
 
-    public function generateReportPdf(Request $request, $recordsPerPartition, $partitionNumber, $skippedData, $filename, $sortBy, $sortOrder)
-    {
-        $startDate = $request->input('startDate');
-        $endDate = $request->input('endDate');
-        $passed = $request->input('passed');
-        $failed = $request->input('failed');
-        $retake = $request->input('retake');
-        $location = $request->input('location');
-
-        $writtenExam = WrittenExam::query();
-
-        $writtenExam->join('erad_tblMain', 'erad_tblMain.acno', '=', 'erad_tblWExam.acno')
-        ->select('erad_tblWExam.*')
-        ->with('erisTblMainWrittenExam');
-
-        $writtenExam->where(function ($query) use ($passed, $failed, $location, $retake) {
-            if ($passed && $failed && $location) {
-                $query->where(function ($q) use ($passed, $failed) {
-                    $q->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%")
-                          ->orWhere('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
-                })->where('erad_tblWExam.numtakes', '>', 1);
-            }
-            elseif($passed && $failed && $retake)
-            {
-                $query->where(function ($q) use ($passed, $failed) {
-                    $q->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%")
-                        ->orWhere('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
-                })
-                ->where('erad_tblWExam.numtakes', '>', '1');
-            }
-            elseif($passed && $failed)
-            {
-                $query->where(function ($q) use ($passed, $failed) {
-                    $q->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%")
-                        ->orWhere('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
-                });
-            }
-            else 
-            {
-                if ($passed) {
-                    $query->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%");
-                }
-
-                if ($failed) {
-                    $query->where('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
-                }
-
-                if ($location) {
-                    $query->where('erad_tblWExam.we_location', $location);
-                }
-
-                if ($retake) {
-                    $query->where('erad_tblWExam.numtakes', '>', '1');
-                }
-            }
-        });
-        if ($startDate && $endDate) 
-        {
-            $writtenExam->whereBetween(DB::raw('CAST(erad_tblWExam.we_date AS DATE)'), [$startDate, $endDate]);
-        }
-
-        $writtenExam->orderBy($sortBy, $sortOrder);
-
-        // getting the data and applying the skipped data and records per partition to get the correct part of the report
-        $writtenExam = $writtenExam->skip($skippedData)->take($recordsPerPartition)->get();
-
-        $pdf = Pdf::loadView('admin.eris.reports.written_exam.report_pdf', [
-            'writtenExam' => $writtenExam,
-        ])
-
-        ->setPaper('a4', 'landscape');
-
-        return $pdf->stream($filename);
-    }
-
+    // generate download links
     public function generateDownloadLinks(Request $request, $sortBy, $sortOrder,)
     {
-        $sortBy = $sortBy ?? 'we_date';
-        $sortOrder = $sortOrder ?? 'desc';
+        $sortBy = $sortBy ?? 'lastname';
+        $sortOrder = $sortOrder ?? 'asc';
 
         $startDate = $request->input('startDate');
         $endDate = $request->input('endDate');
@@ -266,6 +193,9 @@ class WrittenExamReportController extends Controller
         // number of partitions
         $partitionNumber = 0;
 
+        $totalData = $writtenExam->count();
+        $totalParts = ceil($totalData/$recordsPerPartition);
+
         // number of data that will be skipped
         $skippedData = 0;
 
@@ -274,7 +204,7 @@ class WrittenExamReportController extends Controller
 
         // Chunk the results based on the defined limit (don't remove the &$downloadLinks, $recordsPerPartition, $partitionNumber, $skippedData; 
         // the other parameter here is based on your applied filters change it according to your needs)
-        $writtenExam->chunk($recordsPerPartition, function ($partition) use (&$downloadLinks, $recordsPerPartition, &$partitionNumber, &$skippedData, $sortBy, $sortOrder, $startDate, $endDate, $passed, $failed, $retake, $location) {
+        $writtenExam->chunk($recordsPerPartition, function ($partition) use (&$downloadLinks, $recordsPerPartition, &$partitionNumber, &$skippedData, $sortBy, $sortOrder, $startDate, $endDate, $passed, $failed, $retake, $location, $totalParts) {
 
             // calculating how many data should be skipped for this partition
             $skippedData = $recordsPerPartition * $partitionNumber;
@@ -288,7 +218,7 @@ class WrittenExamReportController extends Controller
             // Create a route to handle the download action for each partition
             // don't remove the $recordsPerPartition, $partitionNumber, $skippedData, $filename
             $downloadRoute = route('written-exam-report.generateReportPdf', 
-                                ['recordsPerPartition' => $recordsPerPartition, 'partitionNumber' => $partitionNumber, 'skippedData' => $skippedData, 'filename' => $filename, 'sortBy' => $sortBy, 'sortOrder' => $sortOrder, 'startDate' => $startDate, 'endDate' => $endDate, 'passed' => $passed, 'failed' => $failed, 'retake' => $retake, 'location' => $location]);
+                                ['recordsPerPartition' => $recordsPerPartition, 'partitionNumber' => $partitionNumber, 'skippedData' => $skippedData, 'filename' => $filename, 'sortBy' => $sortBy, 'sortOrder' => $sortOrder, 'startDate' => $startDate, 'endDate' => $endDate, 'passed' => $passed, 'failed' => $failed, 'retake' => $retake, 'location' => $location, 'totalParts' => $totalParts]);
 
             // Store the download link in the array
             $downloadLinks[] = [
@@ -299,5 +229,86 @@ class WrittenExamReportController extends Controller
 
         // Pass the download links to the next download page
         return view('admin.eris.reports.written_exam.download_reports', compact('downloadLinks'));
+    }
+
+    // generate pdf report
+    public function generateReportPdf(Request $request, $totalParts, $recordsPerPartition, $partitionNumber, $skippedData, $filename, $sortBy, $sortOrder)
+    {
+        $startDate = $request->input('startDate');
+        $endDate = $request->input('endDate');
+        $passed = $request->input('passed');
+        $failed = $request->input('failed');
+        $retake = $request->input('retake');
+        $location = $request->input('location');
+
+        $writtenExam = WrittenExam::query();
+
+        $writtenExam->join('erad_tblMain', 'erad_tblMain.acno', '=', 'erad_tblWExam.acno')
+        ->select('erad_tblWExam.*')
+        ->with('erisTblMainWrittenExam');
+
+        $writtenExam->where(function ($query) use ($passed, $failed, $location, $retake) {
+            if ($passed && $failed && $location) {
+                $query->where(function ($q) use ($passed, $failed) {
+                    $q->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%")
+                          ->orWhere('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
+                })->where('erad_tblWExam.numtakes', '>', 1);
+            }
+            elseif($passed && $failed && $retake)
+            {
+                $query->where(function ($q) use ($passed, $failed) {
+                    $q->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%")
+                        ->orWhere('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
+                })
+                ->where('erad_tblWExam.numtakes', '>', '1');
+            }
+            elseif($passed && $failed)
+            {
+                $query->where(function ($q) use ($passed, $failed) {
+                    $q->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%")
+                        ->orWhere('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
+                });
+            }
+            else 
+            {
+                if ($passed) {
+                    $query->where('erad_tblWExam.we_remarks', 'LIKE', "%$passed%");
+                }
+
+                if ($failed) {
+                    $query->where('erad_tblWExam.we_remarks', 'LIKE', "%$failed%");
+                }
+
+                if ($location) {
+                    $query->where('erad_tblWExam.we_location', $location);
+                }
+
+                if ($retake) {
+                    $query->where('erad_tblWExam.numtakes', '>', '1');
+                }
+            }
+        });
+
+        if ($startDate && $endDate) 
+        {
+            $writtenExam->whereBetween(DB::raw('CAST(erad_tblWExam.we_date AS DATE)'), [$startDate, $endDate]);
+        }
+
+        $writtenExam->orderBy($sortBy, $sortOrder);
+
+        // getting the data and applying the skipped data and records per partition to get the correct part of the report
+        $writtenExam = $writtenExam->skip($skippedData)->take($recordsPerPartition)->get();
+
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->getDomPDF()->set_option("enable_php", true);
+        $pdf->loadView('admin.eris.reports.written_exam.report_pdf', [
+            'writtenExam' => $writtenExam,
+            'totalParts' => $totalParts,
+            'partitionNumber' => $partitionNumber,
+        ])
+
+        ->setPaper('a4', 'landscape');
+
+        return $pdf->stream($filename);
     }
 }
--- resources/views/admin/eris/reports/written_exam/report_pdf.blade.php
+++ resources/views/admin/eris/reports/written_exam/report_pdf.blade.php
@@ -33,12 +33,11 @@
             footer {
                 position: fixed;
                 bottom: -20px;
-                right: 20px;
+                left: 20px;
                 text-align: right;
                 font-size: 10px;
-                color: #333;
+                color: black;
             }
-
             table {
                 border-collapse: collapse;
                 padding-left: 10px;
@@ -138,7 +137,7 @@
 
                 <footer>
                     <div class="flex-container">
-                        <div class="">Page <span class="pagenum"></span></div>
+                        Part {{ $partitionNumber }} of {{ $totalParts }}
                     </div>
                 </footer>
             </div>
@@ -217,6 +216,19 @@
                         @endforeach                 
                 </tbody>
             </table>
+
+            {{-- Here's the magic. This MUST be inside body tag. Page count / total, centered at bottom of page --}}
+            <script type="text/php">
+                if (isset($pdf)) {
+                    $text = "Page {PAGE_NUM}  of  {PAGE_COUNT}";
+                    $size = 7;
+                    $font = $fontMetrics->getFont("Verdana");
+                    $width = $fontMetrics->get_text_width($text, $font, $size) / 2;
+                    $x = ($pdf->get_width() - $width);
+                    $y = $pdf->get_height() - 28;
+                    $pdf->page_text($x, $y, $text, $font, $size);
+                }
+            </script>
         </div>
     </body>
 </html>
