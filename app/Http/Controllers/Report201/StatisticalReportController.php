9fe45233f30bf85056751f75a02a90607a4c98df
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-10 20:41:58 +0800

    dualCitizenshipEligibles added

--- app/Http/Controllers/Report201/StatisticalReportController.php
+++ app/Http/Controllers/Report201/StatisticalReportController.php
@@ -586,9 +586,9 @@ class StatisticalReportController extends Controller
             })
             ->count();
 
-        $indigenousEligibles = PersonalData::query()
+        $dualCitizenshipEligibles = PersonalData::query()
             ->where('status', 'Active')
-            ->where('member_of_indigenous_group', 'Yes')
+            ->where('citizenship', 'Dual-Citizenship')
             ->whereHas('cesStatus', function ($query) {
                 $query->where('description', 'LIKE', '%Eli%');
             })
