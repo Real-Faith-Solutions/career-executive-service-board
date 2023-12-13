9714e416cb9bbba3b5119bde8f57e31321d1652c
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-13 20:56:03 +0800

    IDs now uppercase

--- resources/views/admin/201_profiling/view_profile/partials/identification/form.blade.php
+++ resources/views/admin/201_profiling/view_profile/partials/identification/form.blade.php
@@ -26,7 +26,7 @@
                     <div class="sm:gid-cols-1 mb-3 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                         <div class="mb-3">
                             <label for="gsis">GSIS ID No. <sup>*</sup></label>
-                            <input id="gsis" name="gsis" type="text" value="{{ old('gsis') ?? ($identification->gsis ?? '') }}" oninput="validateInput(gsis, 9, 'all')" onkeypress="validateInput(gsis, 9, 'all')" onblur="checkErrorMessage(gsis)">
+                            <input id="gsis" name="gsis" type="text" value="{{ old('gsis') ?? ($identification->gsis ?? '') }}" oninput="validateInput(gsis, 9, 'all'), this.value = this.value.toUpperCase();" onkeypress="validateInput(gsis, 9, 'all')" onblur="checkErrorMessage(gsis)">
                             <p class="input_error text-red-600"></p>
                             @error('gsis')
                                 <span class="invalid" role="alert">
@@ -36,7 +36,7 @@
                         </div>
                         <div class="mb-3">
                             <label for="pagibig">PAG-IBIG ID No.<sup>*</sup></label>
-                            <input id="pagibig" name="pagibig" type="text" value="{{ old('pagibig') ?? ($identification->pagibig ?? '') }}" oninput="validateInput(pagibig, 9, 'all')" onkeypress="validateInput(pagibig, 9, 'all')" onblur="checkErrorMessage(pagibig)">
+                            <input id="pagibig" name="pagibig" type="text" value="{{ old('pagibig') ?? ($identification->pagibig ?? '') }}" oninput="validateInput(pagibig, 9, 'all'), this.value = this.value.toUpperCase();" onkeypress="validateInput(pagibig, 9, 'all')" onblur="checkErrorMessage(pagibig)">
                             <p class="input_error text-red-600"></p>
                             @error('pagibig')
                                 <span class="invalid" role="alert">
@@ -47,7 +47,7 @@
         
                         <div class="mb-3">
                             <label for="philhealt">PHILHEALTH ID No.<sup>*</sup></label>
-                            <input id="philhealth" name="philhealth" type="text" value="{{ old('philhealth') ?? ($identification->philhealth ?? '') }}" oninput="validateInput(philhealth, 9, 'all')" onkeypress="validateInput(philhealth, 9, 'all')" onblur="checkErrorMessage(philhealth)">
+                            <input id="philhealth" name="philhealth" type="text" value="{{ old('philhealth') ?? ($identification->philhealth ?? '') }}" oninput="validateInput(philhealth, 9, 'all'), this.value = this.value.toUpperCase();" onkeypress="validateInput(philhealth, 9, 'all')" onblur="checkErrorMessage(philhealth)">
                             <p class="input_error text-red-600"></p>
                             @error('philhealth')
                                 <span class="invalid" role="alert">
@@ -61,7 +61,7 @@
                     <div class="sm:gid-cols-1 mb-3 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                         <div class="col-md-4">
                             <label for="sss_no">SSS ID No.</label>
-                            <input id="sss_no" name="sss_no" type="text" value="{{ old('sss_no') ?? ($identification->sss_no ?? '') }}" oninput="validateInput(sss_no, 9, 'all')" onkeypress="validateInput(sss_no, 9, 'all')" onblur="checkErrorMessage(sss_no)">
+                            <input id="sss_no" name="sss_no" type="text" value="{{ old('sss_no') ?? ($identification->sss_no ?? '') }}" oninput="validateInput(sss_no, 9, 'all'), this.value = this.value.toUpperCase();" onkeypress="validateInput(sss_no, 9, 'all')" onblur="checkErrorMessage(sss_no)">
                             <p class="input_error text-red-600"></p>
                             @error('sss_no')
                                 <span class="invalid" role="alert">
@@ -71,7 +71,7 @@
                         </div>
                         <div class="col-md-4">
                             <label for="tin">TIN ID No.</label>
-                            <input id="tin" name="tin" type="text" value="{{ old('tin') ?? ($identification->tin ?? '') }}" oninput="validateInput(tin, 9, 'all')" onkeypress="validateInput(tin, 9, 'all')" onblur="checkErrorMessage(tin)">
+                            <input id="tin" name="tin" type="text" value="{{ old('tin') ?? ($identification->tin ?? '') }}" oninput="validateInput(tin, 9, 'all'), this.value = this.value.toUpperCase();" onkeypress="validateInput(tin, 9, 'all')" onblur="checkErrorMessage(tin)">
                             <p class="input_error text-red-600"></p>
                             @error('tin')
                                 <span class="invalid" role="alert">
